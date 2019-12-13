06a7c6d677cc1dd0d32333956b23ad87baaa212b
AntoinePrv <AntoinePrv@users.noreply.github.com>
2019-12-13 18:04:46 -0500

    Document ecole::base

--- libecole/include/ecole/base/environment.hpp
+++ libecole/include/ecole/base/environment.hpp
@@ -10,29 +10,82 @@
 namespace ecole {
 namespace base {
 
+/**
+ * Abstract base class for all observation spaces.
+ *
+ * Observation spaces can be given to environments to parametrize what observations
+ * (or partially observed states) are returned at every transition.
+ * An observation space is intended to extract the observation out of the scip::Model
+ * in any way desired (including caching, scaling...).
+ * An observation on the contrary hand is a purely self contained data class with no
+ * function.
+ *
+ * @tparam Observation the type of the observation extracted by this class.
+ */
 template <typename Observation> struct ObservationSpace {
 	using obs_t = Observation;
 
 	virtual ~ObservationSpace() = default;
-	virtual obs_t get(scip::Model const& model) = 0;
 	virtual std::unique_ptr<ObservationSpace> clone() const = 0;
+
+	/**
+	 * The method called by environments when needing to return an observation.
+	 */
+	virtual obs_t get(scip::Model const& model) = 0;
 };
 
+/**
+ * Abstract base class for all reward spaces.
+ *
+ * Reward spaces can be given to environments to parametrize what rewards are returned
+ * at every transition.
+ */
 struct RewardSpace {
 	using reward_t = double;
 
 	virtual ~RewardSpace() = default;
-	virtual void reset(scip::Model const& model);
-	virtual reward_t get(scip::Model const& model, bool done = false) = 0;
 	virtual std::unique_ptr<RewardSpace> clone() const = 0;
+
+	/**
+	 * The method called by the environment at the begining of every episode (on the
+	 * initial state).
+	 */
+	virtual void reset(scip::Model const& model);
+
+	/**
+	 * The method called by the environment on every new state (after transitioning).
+	 */
+	virtual reward_t get(scip::Model const& model, bool done = false) = 0;
 };
 
+/**
+ * Abstract base class for all termination spaces.
+ *
+ * Termination spaces can be given to environments to parametrize when the environment
+ * terminates (that is, @ref Env::step returns `true` for the `done` flag).
+ */
 struct TerminationSpace {
 	virtual ~TerminationSpace() = default;
 	virtual std::unique_ptr<TerminationSpace> clone() const = 0;
+
+	/**
+	 * The method called by the environment on every new state (after transitioning).
+	 */
 	virtual bool is_done(scip::Model const& model) = 0;
 };
 
+/**
+ * Abstract base class for all environments.
+ *
+ * Environments are the main abstraction exposed by Ecole.
+ * They characterise the Markov Decision Process task to solve.
+ * The interface to environments is meant to be close to that of
+ * [OpenAi Gym](https://gym.openai.com/), with some differences nontheless due to the
+ * requirements of Ecole.
+ *
+ * @tparam Action The type of the action accepted to transition from one state to another.
+ * @tparam Observation The type of the observation returned on every state.
+ */
 template <typename Action, typename Observation> class Env {
 public:
 	using action_t = Action;
@@ -43,10 +96,47 @@ public:
 
 	virtual ~Env() = default;
 
+	/**
+	 * Set the random seed for the environment, hence making its internals deterministic.
+	 *
+	 * The seed is deteministically changed at every new episode (every call to reset) to
+	 * avoid overfitting on a single seed.
+	 * To get the same trajectory at every episode (provided the problem instance and
+	 * sequence of action taken are also unchanged), one has to seed the environment before
+	 * every call to reset.
+	 */
 	seed_t seed(seed_t seed) noexcept;
+
+	/**
+	 * Get the current random seed.
+	 */
 	seed_t seed() const noexcept;
+
+	/**
+	 * Reset the environment to the initial state on the given problem instance.
+	 *
+	 * @return An observation of the new state.
+	 * @return A boolean flag indicating whether the state is terminal.
+	 * @post Unless the (initial) state is also terminal, transitioning (using step) is
+	 *       possible.
+	 */
 	std::tuple<obs_t, bool> reset(scip::Model&& model);
 	std::tuple<obs_t, bool> reset(std::string const& filename);
+
+	/**
+	 * Transition from one state to another.
+	 *
+	 * Take an action on the previously observed state and transition to a new state.
+	 *
+	 * @param action
+	 * @return An observation of the new state.
+	 * @return A scalar reward from the signal to maximize.
+	 * @return A boolean flag indicating whether the state is terminal.
+	 * @return Any additional information about the transition.
+	 * @pre A call to reset must have been done prior to transitioning.
+	 * @pre The envrionment must not be on a terminal state, or have thrown an exception.
+	 *      In such cases, a call to reset must be perform before continuing.
+	 */
 	std::tuple<obs_t, reward_t, bool, info_t> step(action_t action);
 
 private:
