704af15dd43deb6223c0e35118d43317d428718b
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-01-07 14:36:30 -0500

    finish base::RewardSpace Api

--- libecole/src/base/environment.cpp
+++ libecole/src/base/environment.cpp
@@ -6,10 +6,6 @@ namespace base {
 void RewardSpace::reset(scip::Model const& model) {
 	(void)model;
 }
-auto RewardSpace::step(scip::Model const& model) -> reward_t {
-	(void)model;
-	return 0;
-}
 
 }  // namespace base
 }  // namespace ecole
