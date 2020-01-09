f383cbd7db7c702e19c99abf104ec62c3e6568e3
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-01-08 20:14:26 -0500

    Add Python bindings for model parameters

---
+++ python/tests/test_scip.py
@@ -0,0 +1,33 @@
+import pytest
+
+import ecole.scip
+
+
+@pytest.fixture
+def model(problem_file):
+    return ecole.scip.Model.from_file(str(problem_file))
+
+
+names_types = (
+    ("branching/preferbinary", bool),  # Bool param
+    ("conflict/maxlploops", int),  # Int param
+    ("limits/nodes", int),  # Long int param
+    ("limits/time", float),  # Real param
+    ("branching/pscost/strategy", str),  # Char param
+    ("concurrent/paramsetprefix", str),  # String param
+)
+
+
+@pytest.mark.parametrize("name,param_type", names_types)
+def test_get_param(model, name, param_type):
+    assert type(model.get_param(name)) == param_type
+
+
+@pytest.mark.parametrize("name,param_type", names_types)
+def test_set_param(model, name, param_type):
+    if param_type is str:
+        value = "v"  # A value accepted for the Char parameter
+    else:
+        value = param_type(1)  # Cast one to the required type
+    model.set_param(name, value)
+    assert model.get_param(name) == value
