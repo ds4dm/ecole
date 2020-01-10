bc25c323df7a305ce6bfd0ecae574b54feec223b
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-01-10 15:39:28 -0500

    Add Python bindings for TerminationSpace.reset and ObservationSpace.reset

--- python/tests/test_observation.py
+++ python/tests/test_observation.py
@@ -4,5 +4,6 @@ import ecole.observation as O
 def test_BasicObsSpace(model):
     obs_space = O.BasicObsSpace()
     assert isinstance(obs_space, O.BasicObsSpace)
+    obs_space.reset(model)
     obs = obs_space.get(model)
     assert isinstance(obs, O.BasicObs)
