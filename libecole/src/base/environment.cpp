fd40734ee96a3bc9332294b6be4218d7f00cac1f
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-01-10 15:17:52 -0500

    Add reset for all base spaces

--- libecole/src/base/environment.cpp
+++ libecole/src/base/environment.cpp
@@ -7,5 +7,9 @@ void RewardSpace::reset(scip::Model const& model) {
 	(void)model;
 }
 
+void TerminationSpace::reset(scip::Model const& model) {
+	(void)model;
+}
+
 }  // namespace base
 }  // namespace ecole
