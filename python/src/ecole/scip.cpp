162e11788caefdff648579649ebf794551a725a1
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-03-04 11:15:17 -0500

    Fix scip.Model bindings

--- python/src/ecole/scip.cpp
+++ python/src/ecole/scip.cpp
@@ -8,6 +8,28 @@
 namespace py11 = pybind11;
 using namespace ecole;
 
+// Specialize Model get_paramter method for dynamic return type
+template <> py11::object scip::Model::get_param(std::string const& name) const {
+	switch (get_param_type(name)) {
+	case ParamType::Bool:
+		return py11::cast(get_param_explicit<ParamType::Bool>(name));
+	case ParamType::Int:
+		return py11::cast(get_param_explicit<ParamType::Int>(name));
+	case ParamType::LongInt:
+		return py11::cast(get_param_explicit<ParamType::LongInt>(name));
+	case ParamType::Real:
+		return py11::cast(get_param_explicit<ParamType::Real>(name));
+	case ParamType::Char:
+		return py11::cast(get_param_explicit<ParamType::Char>(name));
+	case ParamType::String:
+		return py11::cast(get_param_explicit<ParamType::String>(name));
+	default:
+		assert(false);  // All enum value should be handled
+		// Non void return for optimized build
+		throw Exception("Could not find type for given parameter");
+	}
+}
+
 PYBIND11_MODULE(scip, m) {
 	m.doc() = "Scip wrappers for ecole.";
 
@@ -21,28 +43,28 @@ PYBIND11_MODULE(scip, m) {
 
 		.def(
 			"get_param",
-			[](scip::Model const& model, const char* name) {
+			[](scip::Model const& model, std::string const& name) {
 				// Use the variant to bind
 				return model.get_param<py11::object>(name);
 			})
 		.def(
 			"set_param",
-			[](scip::Model& model, const char* name, py11::bool_ value) {
+			[](scip::Model& model, std::string const& name, py11::bool_ value) {
 				model.set_param(name, value);
 			})
 		.def(
 			"set_param",
-			[](scip::Model& model, const char* name, py11::int_ value) {
+			[](scip::Model& model, std::string const& name, py11::int_ value) {
 				model.set_param(name, value);
 			})
 		.def(
 			"set_param",
-			[](scip::Model& model, const char* name, py11::float_ value) {
+			[](scip::Model& model, std::string const& name, py11::float_ value) {
 				model.set_param(name, value);
 			})
 		.def(
 			"set_param",
-			[](scip::Model& model, const char* name, const char* value) {
+			[](scip::Model& model, std::string const& name, std::string const& value) {
 				model.set_param(name, value);
 			})
 		.def("disable_cuts", &scip::Model::disable_cuts)
