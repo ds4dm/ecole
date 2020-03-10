d7b6dd29491dc7654acb4714aadc2b9c37d7409e
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-03-10 16:42:30 -0400

    Add columns features to NodeBipartite

--- libecole/src/observation/node-bipartite.cpp
+++ libecole/src/observation/node-bipartite.cpp
@@ -5,30 +5,56 @@
 #include <xtensor/xview.hpp>
 
 #include "ecole/observation/node-bipartite.hpp"
+#include "ecole/scip/type.hpp"
 
 namespace ecole {
 namespace observation {
-
-template <typename T> static constexpr T nan() {
-	return std::numeric_limits<T>::quiet_NaN();
-}
 
 auto NodeBipartite::clone() const -> std::unique_ptr<Base> {
 	return std::make_unique<NodeBipartite>(*this);
 }
 
-auto NodeBipartite::get(environment::State const& state) -> NodeBipartiteObs {
-	auto const extract_feat = [](auto const var, auto out_iter) {
-		*out_iter = var.ub_local().value_or(nan<double>());
-	};
+using tensor = decltype(NodeBipartiteObs::col_feat);
+using value_type = tensor::value_type;
+constexpr auto nan = std::numeric_limits<value_type>::quiet_NaN();
 
-	auto var_feat = decltype(NodeBipartiteObs::col_feat)::from_shape(
-		std::array<std::size_t, 2>{state.model.variables().size, 1});
-	std::size_t idx = 0;
-	for (auto var : state.model.variables()) {
-		extract_feat(var, xt::view(var_feat, idx++).begin());
+constexpr std::size_t n_col_feat() {
+	return (
+		11 + scip::enum_size<scip::var_type>::value +
+		scip::enum_size<scip::base_stat>::value);
+}
+
+static auto extract_col_feat(scip::Model const& model) {
+	tensor col_feat{{model.variables().size, n_col_feat()}, 0.};
+
+	auto iter = col_feat.begin();
+	for (auto col : model.lp_columns()) {
+		*(iter++) = static_cast<value_type>(col.lb().value_or(nan));
+		*(iter++) = static_cast<value_type>(col.ub().value_or(nan));
+		*(iter++) = static_cast<value_type>(col.reduced_cost());
+		*(iter++) = static_cast<value_type>(col.obj());
+		*(iter++) = static_cast<value_type>(col.prim_sol());
+		*(iter++) = static_cast<value_type>(col.prim_sol_frac());
+		*(iter++) = static_cast<value_type>(col.is_prim_sol_at_lb());
+		*(iter++) = static_cast<value_type>(col.is_prim_sol_at_ub());
+		*(iter++) = static_cast<value_type>(col.age());
+		iter[static_cast<std::size_t>(col.basis_status())] = 1.;
+		iter += scip::enum_size<scip::base_stat>::value;
+		auto const var = col.var();
+		*(iter++) = static_cast<value_type>(var.best_sol_val().value_or(nan));
+		*(iter++) = static_cast<value_type>(var.avg_sol().value_or(nan));
+		iter[static_cast<std::size_t>(var.type_())] = 1.;
+		iter += scip::enum_size<scip::var_type>::value;
 	}
-	return {std::move(var_feat)};
+
+	// Make sure we iterated over as many element as there are in the tensor
+	assert(static_cast<std::size_t>(iter - col_feat.begin()) == col_feat.size());
+
+	return col_feat;
+}
+
+auto NodeBipartite::get(environment::State const& state) -> NodeBipartiteObs {
+	return {extract_col_feat(state.model)};
 }
 
 }  // namespace observation
