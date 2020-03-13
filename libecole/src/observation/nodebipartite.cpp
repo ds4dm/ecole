2c457f46205731b7595455d9625123050b56a7e0
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-03-13 18:06:01 -0400

    Adapte NodeBipartite Col Row feature to match learn2branch

--- libecole/src/observation/nodebipartite.cpp
+++ libecole/src/observation/nodebipartite.cpp
@@ -17,28 +17,40 @@ auto NodeBipartite::clone() const -> std::unique_ptr<Base> {
 using tensor = decltype(NodeBipartiteObs::col_feat);
 using value_type = tensor::value_type;
 
-constexpr static auto nan = std::numeric_limits<value_type>::quiet_NaN();
-constexpr static auto n_row_feat = 13 + scip::enum_size<scip::base_stat>::value;
-constexpr static auto n_col_feat =
+static value_type constexpr cste = 5.;
+static value_type constexpr nan = std::numeric_limits<value_type>::quiet_NaN();
+static auto constexpr n_row_feat = 5;
+static auto constexpr n_col_feat =
 	11 + scip::enum_size<scip::var_type>::value + scip::enum_size<scip::base_stat>::value;
+
+static value_type get_obj_norm(scip::Model const& model) {
+	auto norm = SCIPgetObjNorm(model.get_scip_ptr());
+	return norm > 0 ? norm : 1.;
+}
 
 static auto extract_col_feat(scip::Model const& model) {
 	tensor col_feat{{model.lp_columns().size, n_col_feat}, 0.};
 
+	value_type const n_lps = SCIPgetNLPs(model.get_scip_ptr());
+	value_type const obj_l2_norm = get_obj_norm(model);
+
 	auto iter = col_feat.begin();
-	for (auto col : model.lp_columns()) {
-		*(iter++) = static_cast<value_type>(col.lb().value_or(nan));
-		*(iter++) = static_cast<value_type>(col.ub().value_or(nan));
-		*(iter++) = static_cast<value_type>(col.reduced_cost());
-		*(iter++) = static_cast<value_type>(col.obj());
+	for (auto const col : model.lp_columns()) {
+		auto const var = col.var();
+		*(iter++) = static_cast<value_type>(col.lb().has_value());
+		*(iter++) = static_cast<value_type>(col.ub().has_value());
+		*(iter++) = static_cast<value_type>(col.reduced_cost()) / obj_l2_norm;
+		*(iter++) = static_cast<value_type>(col.obj()) / obj_l2_norm;
 		*(iter++) = static_cast<value_type>(col.prim_sol());
-		*(iter++) = static_cast<value_type>(col.prim_sol_frac());
+		if (var.type_() == SCIP_VARTYPE_CONTINUOUS)
+			*(iter++) = 0.;
+		else
+			*(iter++) = static_cast<value_type>(col.prim_sol_frac());
 		*(iter++) = static_cast<value_type>(col.is_prim_sol_at_lb());
 		*(iter++) = static_cast<value_type>(col.is_prim_sol_at_ub());
-		*(iter++) = static_cast<value_type>(col.age());
+		*(iter++) = static_cast<value_type>(col.age()) / (n_lps + cste);
 		iter[static_cast<std::size_t>(col.basis_status())] = 1.;
 		iter += scip::enum_size<scip::base_stat>::value;
-		auto const var = col.var();
 		*(iter++) = static_cast<value_type>(var.best_sol_val().value_or(nan));
 		*(iter++) = static_cast<value_type>(var.avg_sol().value_or(nan));
 		iter[static_cast<std::size_t>(var.type_())] = 1.;
@@ -51,28 +63,46 @@ static auto extract_col_feat(scip::Model const& model) {
 	return col_feat;
 }
 
+/**
+ * Number of inequality rows.
+ *
+ * Row are counted once per right hand side and once per left hand side.
+ */
+static std::size_t get_n_ineq_rows(scip::Model const& model) {
+	std::size_t count = 0;
+	for (auto row : model.lp_rows()) {
+		count += static_cast<std::size_t>(row.rhs().has_value());
+		count += static_cast<std::size_t>(row.lhs().has_value());
+	}
+	return count;
+}
+
 static auto extract_row_feat(scip::Model const& model) {
-	tensor row_feat{{model.lp_rows().size, n_row_feat}, 0.};
+	tensor row_feat{{get_n_ineq_rows(model), n_row_feat}, 0.};
+
+	value_type const n_lps = SCIPgetNLPs(model.get_scip_ptr());
+	value_type const obj_l2_norm = get_obj_norm(model);
+
+	auto extract_row = [n_lps, obj_l2_norm](auto& iter, auto const row, bool const lhs) {
+		value_type const sign = lhs ? -1. : 1.;
+		value_type const row_l2_norm = static_cast<value_type>(row.l2_norm());
+		if (lhs) {
+			*(iter++) = row.lhs().value() / row_l2_norm;
+			*(iter++) = static_cast<value_type>(row.is_at_lhs());
+		} else {
+			*(iter++) = row.rhs().value() / row_l2_norm;
+			*(iter++) = static_cast<value_type>(row.is_at_rhs());
+		}
+		*(iter++) = static_cast<value_type>(row.age()) / (n_lps + cste);
+		*(iter++) = sign * row.obj_cos_sim();
+		*(iter++) = sign * row.dual_sol() / (row_l2_norm * obj_l2_norm);
+	};
 
 	auto iter = row_feat.begin();
-	auto count = 0;
-	for (auto row : model.lp_rows()) {
-		*(iter++) = static_cast<value_type>(row.rhs().value_or(nan));
-		*(iter++) = static_cast<value_type>(row.lhs().value_or(nan));
-		*(iter++) = static_cast<value_type>(row.n_lp_nonz());
-		*(iter++) = static_cast<value_type>(row.dual_sol());
-		*(iter++) = static_cast<value_type>(row.age());
-		*(iter++) = static_cast<value_type>(row.lp_activity());
-		*(iter++) = static_cast<value_type>(row.obj_cos_sim());
-		*(iter++) = static_cast<value_type>(row.l2_norm());
-		*(iter++) = static_cast<value_type>(row.is_at_rhs());
-		*(iter++) = static_cast<value_type>(row.is_at_lhs());
-		*(iter++) = static_cast<value_type>(row.is_local());
-		*(iter++) = static_cast<value_type>(row.is_modifiable());
-		*(iter++) = static_cast<value_type>(row.is_removable());
-		iter[static_cast<std::size_t>(row.basis_status())] = 1.;
-		iter += scip::enum_size<scip::base_stat>::value;
-		count++;
+	for (auto const row : model.lp_rows()) {
+		// Rows are counted once per rhs and once per lhs
+		if (row.lhs().has_value()) extract_row(iter, row, true);
+		if (row.rhs().has_value()) extract_row(iter, row, false);
 	}
 
 	// Make sure we iterated over as many element as there are in the tensor
