4e42ffe80a65b4e983333b0a53d9d959945000e2
Maxime Gasse <maxime.gasse@gmail.com>
2020-04-03 10:20:40 -0400

    Moved private parts last

--- libecole/include/ecole/reward/nlpiterations.hpp
+++ libecole/include/ecole/reward/nlpiterations.hpp
@@ -8,12 +8,13 @@ namespace ecole {
 namespace reward {
 
 class NLPIterations : public RewardFunction {
-	SCIP_Longint counter;
-
 public:
 	std::unique_ptr<RewardFunction> clone() const override;
 	void reset(environment::State const& initial_state) override;
 	Reward get(environment::State const& state, bool done = false) override;
+
+private:
+    scip::long_int last_lp_iter;
 };
 
 }  // namespace reward
