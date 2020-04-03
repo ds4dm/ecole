e2c301c2a01135047b7020de06e08240da3ceb2c
Maxime Gasse <maxime.gasse@gmail.com>
2020-04-03 10:22:09 -0400

    Renamed counter as last_lp_iter

--- libecole/src/reward/nlpiterations.cpp
+++ libecole/src/reward/nlpiterations.cpp
@@ -8,14 +8,14 @@ auto NLPIterations::clone() const -> std::unique_ptr<RewardFunction> {
 }
 
 void NLPIterations::reset(environment::State const& initial_state) {
-	counter = SCIPgetNLPIterations(initial_state.model.get_scip_ptr());
+	last_lp_iter = SCIPgetNLPIterations(initial_state.model.get_scip_ptr());
 }
 
 Reward NLPIterations::get(environment::State const& state, bool done) {
 	(void)done;
-	auto reward = SCIPgetNLPIterations(state.model.get_scip_ptr()) - counter;
-	counter += reward;
-	return static_cast<double>(reward);
+	auto lp_iter_diff = SCIPgetNLPIterations(state.model.get_scip_ptr()) - last_lp_iter;
+	last_lp_iter += lp_iter_diff;
+	return static_cast<double>(lp_iter_diff);
 }
 
 }  // namespace reward
