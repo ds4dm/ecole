da0611a86ec583cc48c227568da413d3ed01be7e
Maxime Gasse <maxime.gasse@gmail.com>
2020-04-09 17:24:48 -0400

    C++ formatting

--- libecole/tests/src/reward/test-lpiterations.cpp
+++ libecole/tests/src/reward/test-lpiterations.cpp
@@ -13,34 +13,34 @@
 using namespace ecole;
 
 TEST_CASE("Using the reward in a Configuring environment") {
-	auto env =
-		environment::Configuring<observation::None, reward::LPIterations, termination::WhenSolved>(
+	auto env = environment::
+		Configuring<observation::None, reward::LPIterations, termination::WhenSolved>(
 			{}, {}, {});
 
 	for (auto i = 0; i < 2; ++i) {
-        env.seed(i);
+		env.seed(i);
 		env.reset(problem_file);
 
 		auto obs_rew_done_info = env.step({});
-        auto reward = std::get<1>(obs_rew_done_info);
+		auto reward = std::get<1>(obs_rew_done_info);
 
-        // Assert that the reward is non-positive
+		// Assert that the reward is non-positive
 		REQUIRE(reward <= 0);
 	}
 
 	for (auto i = 0; i < 2; ++i) {
-        env.seed(i);
+		env.seed(i);
 		env.reset(problem_file);
 
 		auto obs_rew_done_info = env.step({
-            {"presolving/maxrounds", 0},
+			{"presolving/maxrounds", 0},
 			{"lp/iterlim", 0},
-            {"lp/rootiterlim", 0},
+			{"lp/rootiterlim", 0},
 			{"limits/totalnodes", 1},
 		});
-        auto reward = std::get<1>(obs_rew_done_info);
+		auto reward = std::get<1>(obs_rew_done_info);
 
-        // Assert that the reward is zero, since no LP iteration was allowed
+		// Assert that the reward is zero, since no LP iteration was allowed
 		REQUIRE(reward == 0);
 	}
 }
