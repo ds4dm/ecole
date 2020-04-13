0ea9b36bfc19271c99560facaf1876818917ffbf
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-04-13 10:24:23 -0400

    Only create CMake venv at config time

---
+++ cmake/Python.cmake
@@ -0,0 +1,37 @@
+# Prefer finding Python from path if possible
+if(NOT DEFINED Python_ROOT_DIR)
+	execute_process(
+		COMMAND which python
+		COMMAND xargs dirname  # Pipe
+		OUTPUT_VARIABLE Python_ROOT_DIR
+		RESULTS_VARIABLE Python_IN_PATH
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+endif()
+
+# Find the Python Interpreter
+find_package(Python COMPONENTS Interpreter REQUIRED)
+# Use FindPython to override wrong PyBind resolution
+set(PYTHON_EXECUTABLE ${Python_EXECUTABLE})
+
+# Create a virtual environment (at configure time) where Ecole can safely be installed
+set(VENV_DIR "${CMAKE_CURRENT_BINARY_DIR}/venv")
+execute_process(
+	COMMAND "${Python_EXECUTABLE}" -m venv --system-site-package "${VENV_DIR}"
+	RESULT_VARIABLE VENV_ERROR
+	ERROR_VARIABLE VENV_ERROR_MESSAGE
+)
+
+# Report on virtual environment creation
+if(VENV_ERROR)
+	message(SEND_ERROR "${VENV_ERROR_MESSAGE}")
+else()
+	message(STATUS "Created Python virtual environment")
+endif()
+
+# Add a target for the Python of the virtual environment
+add_executable(venv-python IMPORTED)
+set_target_properties(
+	venv-python
+	PROPERTIES IMPORTED_LOCATION "${VENV_DIR}/bin/python"
+)
