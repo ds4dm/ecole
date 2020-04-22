74dd1985c04952c4e389e6d70755d9f0c21e1e09
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-04-22 10:11:23 -0400

    Add optional and default State functions in Environment.py

--- python/src/ecole/environment.py
+++ python/src/ecole/environment.py
@@ -1,33 +1,34 @@
 """Ecole collection of environments."""
 
-import ecole.core as core
+import enum
+from functools import partial
 
+import ecole.core as core
+import ecole.observation
+import ecole.reward
+import ecole.termination
 from ecole.core.environment import *
 
 
-class NoneFunction:
+class ConstantFunction:
     """A State function to return None.
 
-    An instance of this mock class return `None` on calls, and itself on
-    attribute access.
+    An instance of this mock class return the same value on calls, and itself
+    on attribute access.
     This is convenient to replace any State function, as any call such as
     ```
     self.reset_something(some, arguments)
     ```
-    will return `None`.
+    will return the defined value.
     """
 
-    @classmethod
-    def as_default_of(cls, func):
-        """Substitute the input with a new instance if the it is None."""
-        if func is None:
-            return cls()
-        else:
-            return func
+    def __init__(self, return_value):
+        """Define the value to return."""
+        self.return_value = return_value
 
     def __call__(self, *args, **kwargs):
         """Return None."""
-        return None
+        return self.return_value
 
     def __getattr__(self, attr):
         """Return itself."""
@@ -38,16 +39,40 @@ class EnvironmentComposer:
 
     __Dynamics__ = None
     __State__ = None
+    __DefaultObservationFunction__ = partial(ConstantFunction, None)
+    __DefaultRewardFunction__ = partial(ConstantFunction, 0)
+    __DefaultTerminationFunction__ = partial(ConstantFunction, False)
 
     def __init__(
         self,
-        observation_function=None,
-        reward_function=None,
-        termination_function=None,
+        observation_function="default",
+        reward_function="default",
+        termination_function="default",
     ) -> None:
-        self.observation_function = NoneFunction.as_default_of(observation_function)
-        self.reward_function = NoneFunction.as_default_of(reward_function)
-        self.termination_function = NoneFunction.as_default_of(termination_function)
+        #  Set observation function
+        if observation_function == "default":
+            self.observation_function = self.__DefaultObservationFunction__()
+        elif observation_function is None:
+            self.observation_function = ConstantFunction(None)
+        else:
+            self.observation_function = observation_function
+
+        #  Set reward function
+        if reward_function == "default":
+            self.reward_function = self.__DefaultRewardFunction__()
+        elif reward_function is None:
+            self.reward_function = ConstantFunction(0)
+        else:
+            self.reward_function = reward_function
+
+        #  Set termination function
+        if termination_function == "default":
+            self.termination_function = self.__DefaultTerminationFunction__()
+        elif termination_function is None:
+            self.termination_function = ConstantFunction(False)
+        else:
+            self.termination_function = termination_function
+
         self.state = None
         self.dynamics = self.__Dynamics__()
         self.can_transition = False
@@ -65,11 +90,11 @@ class EnvironmentComposer:
             self.observation_function.reset(self.state)
             self.reward_function.reset(self.state)
 
-            done = done or bool(self.termination_function.is_done(self.state))
+            done = done or self.termination_function.is_done(self.state)
             if done:
-                observation = self.observation_function.get(self.state)
-            else:
                 observation = None
+            else:
+                observation = self.observation_function.get(self.state)
             return observation, done
         except Exception as e:
             self.can_transition = False
@@ -81,12 +106,12 @@ class EnvironmentComposer:
 
         try:
             done = self.dynamics.step_dynamics(self.state, action)
-            done = done or bool(self.termination_function.is_done(self.state))
+            done = done or self.termination_function.is_done(self.state)
             reward = self.reward_function.get(self.state, done)
             if done:
-                observation = self.observation_function.get(self.state)
-            else:
                 observation = None
+            else:
+                observation = self.observation_function.get(self.state)
             return observation, reward, done, {}
         except Exception as e:
             self.can_transition = False
@@ -96,8 +121,11 @@ class EnvironmentComposer:
 class Branching(EnvironmentComposer):
     __Dynamics__ = core.environment.BranchingDynamics
     __State__ = core.environment.ReverseControlState
+    __DefaultObservationFunction__ = ecole.observation.NodeBipartite
+    __DefaultRewardFunction__ = ecole.reward.IsDone
 
 
 class Configuring(EnvironmentComposer):
     __Dynamics__ = core.environment.ConfiguringDynamics
     __State__ = core.environment.State
+    __DefaultRewardFunction__ = ecole.reward.IsDone
