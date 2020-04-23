daee18f067ac36cf0d88dfba64535b6bee302e81
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-04-23 15:27:37 -0400

    Fix NegLPIterations on invalid SCIP stage

--- libecole/src/reward/neglpiterations.cpp
+++ libecole/src/reward/neglpiterations.cpp
@@ -3,12 +3,25 @@
 namespace ecole {
 namespace reward {
 
+static auto n_lp_iterations(scip::Model const& model) {
+	switch (model.get_stage()) {
+	// Only stages when the following call is authorized
+	case SCIP_STAGE_PRESOLVING:
+	case SCIP_STAGE_PRESOLVED:
+	case SCIP_STAGE_SOLVING:
+	case SCIP_STAGE_SOLVED:
+		return SCIPgetNLPIterations(model.get_scip_ptr());
+	default:
+		return decltype(SCIPgetNLPIterations(nullptr)){0};
+	}
+}
+
 void NegLPIterations::reset(environment::State const& initial_state) {
-	last_lp_iter = SCIPgetNLPIterations(initial_state.model.get_scip_ptr());
+	last_lp_iter = n_lp_iterations(initial_state.model);
 }
 
 Reward NegLPIterations::get(environment::State const& state, bool) {
-	auto lp_iter_diff = SCIPgetNLPIterations(state.model.get_scip_ptr()) - last_lp_iter;
+	auto lp_iter_diff = n_lp_iterations(state.model) - last_lp_iter;
 	last_lp_iter += lp_iter_diff;
 	return static_cast<double>(-lp_iter_diff);
 }
