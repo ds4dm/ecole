3a360df25366f1fa32056ccde6e5bed8be9176c5
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-04-29 12:26:23 -0400

    Upgrade Catch2

--- libecole/tests/src/scip/test-variable.cpp
+++ libecole/tests/src/scip/test-variable.cpp
@@ -8,7 +8,7 @@ using namespace ecole;
 
 TEST_CASE("Model has variable view iterator") {
 	auto model = get_model();
-	int count = 0;
+	std::size_t count = 0;
 	for (auto var : model.variables()) {
 		count++;
 		var.ub_local();
