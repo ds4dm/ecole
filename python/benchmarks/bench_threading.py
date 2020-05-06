5a7835b8789d6ef0b19aeaab98703bcf7800436e
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-05-05 21:10:13 -0400

    Fix benchmark to use threads

--- python/benchmarks/bench_threading.py
+++ python/benchmarks/bench_threading.py
@@ -1,4 +1,4 @@
-import concurrent.futures as futures
+import threading
 
 import pytest
 
@@ -6,28 +6,48 @@ import ecole.environment
 import ecole.observation
 
 
-def run_environment(model) -> int:
-    model.set_param("limits/totalnodes", 10)
-    env = ecole.environment.Branching(
-        observation_function=ecole.observation.NodeBipartite()
-    )
-    count = 0
-    for _ in range(3):
-        _, action_set, done = env.reset(model)
-        while not done:
-            _, action_set, _, done, _ = env.step(action_set[0])
-            count += 1
-    return count
+# FIXME set seed.
+def run_environment(model):
+    env = ecole.environment.Branching(observation_function=ecole.observation.Nothing())
+    _, action_set, done = env.reset(model)
+    while not done:
+        _, action_set, _, done, _ = env.step(action_set[0])
 
 
-@pytest.mark.parametrize("n_workers", (1, 2, 4, 8))
+@pytest.mark.parametrize("n_threads", (1, 2, 4, 8))
 @pytest.mark.benchmark(group="Branching NodeBipartite")
 @pytest.mark.slow
-def test_mulithread(benchmark, model, n_workers):
-    with futures.ThreadPoolExecutor(max_workers=n_workers) as executor:
-        models = [model.clone() for _ in range(5)]
+def test_environment_mulithread(benchmark, model, n_threads):
+    def setup_threads():
+        models = [model.clone() for _ in range(n_threads)]
+        threads = [threading.Thread(target=run_environment, args=(m,)) for m in models]
+        return (threads,), {}
 
-        @benchmark
-        def run_asynch():
-            submissions = [executor.submit(run_environment, model=m) for m in models]
-            return [s.result() for s in submissions]
+    def run_threads(threads):
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+
+    benchmark.pedantic(run_threads, setup=setup_threads, rounds=5)
+
+
+# FIXME This is not comparable to the previous benchmark because it is not the same
+# branching rule.
+# FIXME set seed.
+@pytest.mark.parametrize("n_threads", (1, 2, 4, 8))
+@pytest.mark.benchmark(group="Solving Model")
+@pytest.mark.slow
+def test_solve_mulithread(benchmark, model, n_threads):
+    def setup_threads():
+        models = [model.clone() for _ in range(n_threads)]
+        threads = [threading.Thread(target=m.solve) for m in models]
+        return (threads,), {}
+
+    def run_threads(threads):
+        for t in threads:
+            t.start()
+        for t in threads:
+            t.join()
+
+    benchmark.pedantic(run_threads, setup=setup_threads, rounds=5)
