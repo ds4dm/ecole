730857f4add88f51303e6fab69081aef3eae4d81
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-06-01 11:38:35 -0400

    Restructure reward function bindings and add docstrings

--- python/src/ecole/core/reward.cpp
+++ python/src/ecole/core/reward.cpp
@@ -7,43 +7,159 @@
 
 #include "core.hpp"
 
+namespace py = pybind11;
+
 namespace ecole {
 namespace reward {
 
-namespace py = pybind11;
+/**
+ * Proxy class for doing arithmetic on reward functions.
+ *
+ * This could be a pure Python class, but it needs to be defined in this module to
+ * be accessible to reward functions operators.
+ */
+struct ArithmeticFunction {
+	py::object operation;
+	py::list functions;
 
-template <typename RewardFunction, typename... Args>
-auto reward_function_class(py::module& m, Args&&... args) {
-	return py::class_<RewardFunction>(m, std::forward<Args>(args)...)  //
-		.def("reset", &RewardFunction::reset, py::arg("state"))
-		.def(
-			"obtain_reward",
-			&RewardFunction::obtain_reward,
-			py::arg("state"),
-			py::arg("done") = false);
-}
+	ArithmeticFunction(py::object operation, py::list functions);
+	void reset(py::object initial_state);
+	Reward obtain_reward(py::object state);
+};
 
+/**
+ * Helper function to bind common methods.
+ */
+template <typename PyClass, typename... Args> void def_reset(PyClass, Args&&...);
+template <typename PyClass, typename... Args> void def_obtain_reward(PyClass, Args&&...);
+template <typename PyClass> void def_operators(PyClass);
+
+/**
+ * Reward module bindings definitions.
+ */
 void bind_submodule(py::module m) {
 	m.doc() = "Reward classes for Ecole.";
 
+	auto constant = py::class_<Constant>(m, "Constant", R"(
+		Constant Reward.
+
+		Always return the value passed in constructor.
+	)");
+	constant.def(py::init<Reward>(), py::arg("constant") = 0.);
+	def_operators(constant);
+	def_reset(constant, "Do nothing.");
+	def_obtain_reward(constant, "Return the constant value.");
+
+	auto arithmetic_function = py::class_<ArithmeticFunction>(
+		m,
+		"ArithmeticFunction"
+		R"(
+		Proxy class for doing arithmetic on reward functions.
+
+		An object of this class is returned by reward functions operators to forward calls
+		to the reward functions parameters of the operator.
+	)");
+	arithmetic_function.def(py::init<py::object, py::list>());
+	def_operators(arithmetic_function);
+	def_reset(arithmetic_function, R"(
+		Reset the reward functions of the operator.
+
+		Call ``reset`` on all reward functions parameters that were used to create this
+		object.
+	)");
+	def_obtain_reward(arithmetic_function, R"(
+		Obtain the reward of result of the operator.
+
+		Call ``obtain_reward`` on all reward functions parameters that were used to create
+		this object and compute the operation on the results.
+	)");
+
+	auto isdone = py::class_<IsDone>(m, "IsDone", "Single reward on terminal states.");
+	isdone.def(py::init<>());
+	def_operators(isdone);
+	def_reset(isdone, "Do nothing.");
+	def_obtain_reward(
+		isdone, "Return 1 if the episode is on a terminal state, 0 otherwise.");
+
+	auto lpiterations = py::class_<LpIterations>(m, "LpIterations", R"(
+		LP Iteration difference.
+
+		The reward is defined as the number of iterations done solving the Linear Program
+		associated with the problem since the previous state.
+	)");
+	lpiterations.def(py::init<>());
+	def_operators(lpiterations);
+	def_reset(lpiterations, "Reset the internal LP iterations count.");
+	def_obtain_reward(lpiterations, R"(
+		Update the internal LP iteration count and return the difference.
+
+		The difference in LP iteration is computed in between calls.
+		)");
+}
+
+/**************************************
+ *  Definition of ArithmeticFunction  *
+ **************************************/
+
+ArithmeticFunction::ArithmeticFunction(py::object operation_, py::list functions_) :
+	operation(std::move(operation_)) {
+	auto const Numbers = py::module::import("numbers").attr("Number");
+	for (auto func : functions_) {
+		if (py::isinstance(func, Numbers)) {
+			functions.append(py::cast(Constant{func.cast<Reward>()}));
+		} else {
+			functions.append(func);
+		}
+	}
+}
+
+void ArithmeticFunction::reset(py::object initial_state) {
+	for (auto obs_func : functions) {
+		obs_func.attr("reset")(initial_state);
+	}
+}
+
+Reward ArithmeticFunction::obtain_reward(py::object state) {
+	py::list rewards{};
+	for (auto obs_func : functions) {
+		rewards.append(obs_func.attr("obtain_reward")(state));
+	}
+	return operation(*rewards).cast<Reward>();
+}
+
+/************************************
+ *  Definition of helper functions  *
+ ************************************/
+
+template <typename PyClass, typename... Args>
+void def_reset(PyClass pyclass, Args&&... args) {
+	pyclass.def(
+		"reset", &PyClass::type::reset, py::arg("state"), std::forward<Args>(args)...);
+}
+
+template <typename PyClass, typename... Args>
+void def_obtain_reward(PyClass pyclass, Args&&... args) {
+	pyclass.def(
+		"obtain_reward",
+		&PyClass::type::obtain_reward,
+		py::arg("state"),
+		py::arg("done") = false,
+		std::forward<Args>(args)...);
+}
+
+template <typename PyClass> void def_operators(PyClass pyclass) {
 	// Import Python standrad modules
-	auto const numbers = py::module::import("numbers");
 	auto const builtins = py::module::import("builtins");
 	auto const math = py::module::import("math");
 
 	// Return a function that wraps rewards functions inside an ArithmeticReward.
 	// The ArithmeticReward is a reward function class that will call the wrapped reward
 	// functions and merge there rewards with the relevant operation (sum, prod, ...)
-	auto const arithmetic_method = [m](auto operation) {
-		auto const ArithmeticFunction = m.attr("ArithmeticFunction");
-		return [ArithmeticFunction, operation](py::args args) {
-			return ArithmeticFunction(operation, args);
-		};
+	auto const arithmetic_method = [](auto operation) {
+		return [operation](py::args args) { return ArithmeticFunction{operation, args}; };
 	};
 
-	struct RewardFunction {};
-	py::class_<RewardFunction> base_reward_function(m, "RewardFunction");
-	base_reward_function
+	pyclass
 		// Binary operators
 		.def("__add__", arithmetic_method(py::eval("lambda x, y: x + y")))
 		.def("__sub__", arithmetic_method(py::eval("lambda x, y: x - y")))
@@ -92,43 +208,12 @@ void bind_submodule(py::module m) {
 		"sin", "exp", "log", "log2", "log10", "sqrt", "cos", "tan", "asin", "acos", "atan",
 		"sinh", "cosh", "tanh", "asinh", "acosh", "atanh"
 	}) {
-		base_reward_function.def(name, arithmetic_method(math.attr(name)));
+		pyclass.def(name, arithmetic_method(math.attr(name)));
 	}
 	// clang-format on
-	base_reward_function.def("apply", [m](py::object self, py::object func) {
-		return m.attr("ArithmeticFunction")(func, py::make_tuple(self));
+	pyclass.def("apply", [](py::object self, py::object func) {
+		return ArithmeticFunction{func, py::make_tuple(self)};
 	});
-
-	py::exec(
-		R"(
-		import numbers
-
-		class ArithmeticFunction(RewardFunction):
-			"""Apply an arithmetic operation on multiple rewards."""
-
-			def __init__(self, operation, functions):
-				self.operation = operation
-				self.functions = [Constant(f) if isinstance(f, numbers.Number) else f for f in functions]
-
-			def reset(self, initial_state):
-				"""Call reset on all reward functions."""
-				for obs_func in self.observation_functions:
-					obs_func.reset(initial_state)
-
-			def obtain_reward(self, state):
-				return self.operation(*(f.obtain_reward(state) for f in self.functions))
-
-			)",
-		m.attr("__dict__"));
-
-	reward_function_class<Constant>(m, "Constant", base_reward_function)  //
-		.def(py::init<Reward>(), py::arg("constant") = 0.);
-
-	reward_function_class<IsDone>(m, "IsDone", base_reward_function)  //
-		.def(py::init<>());
-
-	reward_function_class<LpIterations>(m, "LpIterations", base_reward_function)  //
-		.def(py::init<>());
 }
 
 }  // namespace reward
