dfebcabd5a72c4974946cf075ea986d4805264f9
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-06-18 18:01:34 -0400

    Refactor test-branching to use unit tests

---
+++ libecole/tests/src/environment/test-branching-dynamics.cpp
@@ -0,0 +1,59 @@
+#include <stdexcept>
+#include <tuple>
+
+#include <catch2/catch.hpp>
+#include <xtensor/xmath.hpp>
+#include <xtensor/xsort.hpp>
+
+#include "ecole/environment/branching-dynamics.hpp"
+#include "ecole/environment/exception.hpp"
+
+#include "conftest.hpp"
+#include "environment/unit-tests.hpp"
+
+using namespace ecole;
+
+TEST_CASE("BranchingDynamics unit tests", "[unit][dyn]") {
+	auto const policy = [](auto const& action_set) { return action_set.value()[0]; };
+	environment::unit_tests(environment::BranchingDynamics{false}, policy);
+}
+
+TEST_CASE("BranchingDynamics with pseudo candidates unit tests", "[unit][dyn]") {
+	auto const policy = [](auto const& action_set) { return action_set.value()[0]; };
+	environment::unit_tests(environment::BranchingDynamics{true}, policy);
+}
+
+TEST_CASE("BranchingDynamics functional tests", "[dyn]") {
+	environment::BranchingDynamics dyn{};
+	auto model = get_model();
+	bool done = false;
+	trait::action_set_of_t<environment::BranchingDynamics> action_set;
+
+	SECTION("Return valid action set") {
+		std::tie(std::ignore, action_set) = dyn.reset_dynamics(model);
+		REQUIRE(action_set.has_value());
+		auto const& branch_cands = action_set.value();
+		REQUIRE(branch_cands.size() > 0);
+		REQUIRE(branch_cands.size() < model.lp_columns().size);
+		REQUIRE(xt::all(branch_cands >= 0));
+		REQUIRE(xt::all(branch_cands < model.lp_columns().size));
+		REQUIRE(xt::unique(branch_cands).size() == branch_cands.size());
+	}
+
+	SECTION("Solve instance") {
+		std::tie(done, action_set) = dyn.reset_dynamics(model);
+		while (!done) {
+			REQUIRE(action_set.has_value());
+			std::tie(done, action_set) = dyn.step_dynamics(model, action_set.value()[0]);
+		}
+		REQUIRE(model.is_solved());
+	}
+
+	SECTION("Throw on invalid branching variable") {
+		std::tie(done, action_set) = dyn.reset_dynamics(model);
+		REQUIRE_FALSE(done);
+		REQUIRE(action_set.has_value());
+		auto const action = model.lp_columns().size + 1;
+		REQUIRE_THROWS_AS(dyn.step_dynamics(model, action), environment::Exception);
+	}
+}
