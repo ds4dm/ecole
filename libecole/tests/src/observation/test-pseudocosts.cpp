bfd906625aaf292a05ac2fe6924a1daa0790fa32
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-07-29 10:49:51 -0400

    Refactor test-pseudocosts to use unit tests functions

--- libecole/tests/src/observation/test-pseudocosts.cpp
+++ libecole/tests/src/observation/test-pseudocosts.cpp
@@ -1,77 +1,34 @@
-#include <limits>
-#include <memory>
-#include <stdexcept>
-#include <tuple>
+#include <cmath>
 
 #include <catch2/catch.hpp>
+#include <scip/scip.h>
 
-#include "ecole/environment/branching.hpp"
-#include "ecole/environment/exception.hpp"
 #include "ecole/observation/pseudocosts.hpp"
-#include "ecole/reward/isdone.hpp"
 
 #include "conftest.hpp"
+#include "observation/unit-tests.hpp"
 
 using namespace ecole;
 
-TEST_CASE("Pseudocosts") {
-	environment::Branching<observation::Pseudocosts, reward::IsDone> env{};
+TEST_CASE("Pseudocosts unit tests", "[unit][obs]") {
+	observation::unit_tests(observation::Pseudocosts{});
+}
 
-	SECTION("reset, reset, and delete") {
-		env.reset(problem_file);
-		env.reset(problem_file);
-	}
+TEST_CASE("Pseudocosts return pseudo costs array", "[obs]") {
+	auto obs_func = observation::Pseudocosts{};
+	auto model = get_solving_model();
+	obs_func.reset(model);
+	auto const obs = obs_func.obtain_observation(model);
 
-	SECTION("reset, step, and delete") {
-		decltype(env)::ActionSet action_set;
-		std::tie(std::ignore, action_set, std::ignore, std::ignore) = env.reset(problem_file);
-		env.step(action_set.value()[0]);
-	}
+	REQUIRE(obs.has_value());
+	auto const costs = obs.value();
+	REQUIRE(costs.size() == model.lp_columns().size);
 
-	SECTION("run full trajectory") {
-		auto run_trajectory = [&env](std::string const& filename) {
-			decltype(env)::Observation obs;
-			decltype(env)::ActionSet action_set;
-			bool done = false;
-			reward::Reward reward;
-
-			std::tie(obs, action_set, reward, done) = env.reset(filename);
-			auto count = 0;
-
-			// Assert that the observation is none only on terminal states
-			REQUIRE(obs.has_value() != done);
-
-			while (!done) {
-				auto const action = action_set.value()[0];
-				std::tie(obs, action_set, reward, done, std::ignore) = env.step(action);
-				++count;
-
-				// Assert that the observation is none only on terminal states
-				REQUIRE(obs.has_value() != done);
-				if (obs.has_value()) {
-					auto const scip = env.model().get_scip_ptr();
-
-					auto const nb_observations = static_cast<int>(obs.value().shape()[0]);
-					REQUIRE(nb_observations == SCIPgetNVars(scip));
-
-					SCIP_VAR** cands;
-					int nb_cands;
-					SCIPgetLPBranchCands(scip, &cands, NULL, NULL, NULL, &nb_cands, NULL);
-
-					for (int i = 0; i < nb_cands; i++) {
-						auto const lp_index = SCIPcolGetLPPos(SCIPvarGetCol(cands[i]));
-						auto pseudocost = obs.value()(lp_index);
-
-						REQUIRE(pseudocost != std::nan(""));
-						REQUIRE(pseudocost > 0);
-					}
-				}
-			}
-
-			REQUIRE(count > 0);
-		};
-		run_trajectory(problem_file);
-
-		SECTION("Run another trajectory") { run_trajectory(problem_file); }
+	// All branching candidates have a positive pseudocost
+	for (auto const var : model.lp_branch_cands()) {
+		auto const lp_index = static_cast<std::size_t>(SCIPcolGetLPPos(SCIPvarGetCol(var.value)));
+		auto const pseudocost = costs[lp_index];
+		REQUIRE(!std::isnan(pseudocost));
+		REQUIRE(pseudocost > 0);
 	}
 }
