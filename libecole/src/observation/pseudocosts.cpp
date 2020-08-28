8ef257feba7bb78feafdd131f4fed9d4513ca8a7
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-08-28 18:54:59 -0400

    Use ranges in Pseudocosts

--- libecole/src/observation/pseudocosts.cpp
+++ libecole/src/observation/pseudocosts.cpp
@@ -2,6 +2,8 @@
 #include <cstddef>
 #include <optional>
 
+#include <nonstd/span.hpp>
+#include <range/v3/view/zip.hpp>
 #include <xtensor/xtensor.hpp>
 #include <xtensor/xview.hpp>
 
@@ -11,29 +13,39 @@
 
 namespace ecole::observation {
 
+namespace views = ranges::views;
+
+namespace {
+
+/** get vanilla full strong branching scores and variables */
+auto scip_get_lp_branch_cands(Scip* const scip) noexcept {
+	SCIP_VAR** cands = nullptr;
+	SCIP_Real* cands_lp_values = nullptr;
+	int n_cands = 0;
+	SCIPgetLPBranchCands(scip, &cands, &cands_lp_values, nullptr, nullptr, &n_cands, nullptr);
+	return std::tuple{
+		nonstd::span{cands, static_cast<std::size_t>(n_cands)},
+		nonstd::span{cands_lp_values, static_cast<std::size_t>(n_cands)},
+	};
+}
+
+}  // namespace
+
 std::optional<xt::xtensor<double, 1>> Pseudocosts::obtain_observation(scip::Model& model) {
 	if (model.get_stage() != SCIP_STAGE_SOLVING) {
 		return {};
 	}
 
 	auto* const scip = model.get_scip_ptr();
-
-	/* get branching candidates */
-	SCIP_VAR** cands;
-	SCIP_Real* cands_lp_values;
-	int nb_cands;
-
-	SCIPgetLPBranchCands(scip, &cands, &cands_lp_values, nullptr, nullptr, &nb_cands, nullptr);
-	assert(nb_cands > 0);
+	auto const [cands, lp_values] = scip_get_lp_branch_cands(scip);
 
 	/* Store pseudocosts in tensor */
 	auto const nb_lp_columns = static_cast<std::size_t>(SCIPgetNLPCols(scip));
 	xt::xtensor<double, 1> pseudocosts({nb_lp_columns}, std::nan(""));
 
-	for (std::size_t i = 0; i < static_cast<std::size_t>(nb_cands); i++) {
-		auto* const col = SCIPvarGetCol(cands[i]);
-		auto const lp_index = static_cast<std::size_t>(SCIPcolGetLPPos(col));
-		auto const score = SCIPgetVarPseudocostScore(scip, cands[i], cands_lp_values[i]);
+	for (auto const [var, lp_val] : views::zip(cands, lp_values)) {
+		auto const lp_index = static_cast<std::size_t>(SCIPcolGetLPPos(SCIPvarGetCol(var)));
+		auto const score = SCIPgetVarPseudocostScore(scip, var, lp_val);
 		pseudocosts[lp_index] = static_cast<double>(score);
 	}
 
