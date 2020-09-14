c54765559b94ce358cc60f371d0ff27cc24d0975
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-09-14 11:56:32 -0400

    Enable IPO

---
+++ cmake/InterproceduralOptimization.cmake
@@ -0,0 +1,12 @@
+option(ENABLE_IPO "Enable Interprocedural Optimization, aka Link Time Optimization (LTO)" OFF)
+
+if(ENABLE_IPO)
+	include(CheckIPOSupported)
+	check_ipo_supported(RESULT result OUTPUT output)
+	if(result)
+		set(CMAKE_INTERPROCEDURAL_OPTIMIZATION TRUE)
+		message(STATUS "IPO enabled")
+	else()
+		message(SEND_ERROR "IPO is not supported: ${output}")
+	endif()
+endif()
