53218a3c6f7c6357737317f547bbf3049ae0516b
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-09-30 15:45:31 -0400

    Instance gen bindings with Paramters unpacking

--- libecole/src/instance/set-cover.cpp
+++ libecole/src/instance/set-cover.cpp
@@ -2,19 +2,19 @@
 
 namespace ecole::instance {
 
-scip::Model SetCoverGenerator::generate_instance(Parameters /*parameters*/, RandomEngine& /*random_engine*/) {
+scip::Model SetCoverGenerator::generate_instance(RandomEngine& /*random_engine*/, Parameters /*parameters*/) {
 	// TODO implement algorithm
 	return {};
 }
 
-SetCoverGenerator::SetCoverGenerator(Parameters parameters_, RandomEngine random_engine_) :
+SetCoverGenerator::SetCoverGenerator(RandomEngine random_engine_, Parameters parameters_) :
 	random_engine{random_engine_}, parameters{parameters_} {}
 SetCoverGenerator::SetCoverGenerator(Parameters parameters) :
-	SetCoverGenerator{parameters, RandomEngine{std::random_device{}()}} {}
+	SetCoverGenerator{RandomEngine{std::random_device{}()}, parameters} {}
 SetCoverGenerator::SetCoverGenerator() : SetCoverGenerator(Parameters{}) {}
 
 scip::Model SetCoverGenerator::next() {
-	return generate_instance(parameters, random_engine);
+	return generate_instance(random_engine, parameters);
 }
 
 }  // namespace ecole::instance
