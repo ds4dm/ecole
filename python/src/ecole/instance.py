05f7a5d50bafdcafadd12539e18e45b79857fd04
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-10-06 14:45:55 -0400

    Switch ecole.instance for C++

--- python/src/ecole/instance.py
+++ python/src/ecole/instance.py
@@ -1,4 +1,1 @@
-from ecole._set_cover_generator import SetCoverGenerator
-from ecole._combinatorial_auction_generator import CombinatorialAuctionGenerator
-from ecole._capacitated_facility_location_generator import CapacitatedFacilityLocationGenerator
-from ecole._independent_set_generator import IndependentSetGenerator
+from ecole.core.instance import *
