9eb33e5bf9c1662b48d6ca64ab8ae8c7736d2e3e
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-10-06 15:27:07 -0400

    Add CombinatorialAuction C++ skelleton

--- python/tests/test_instance.py
+++ python/tests/test_instance.py
@@ -23,7 +23,6 @@ def pytest_generate_tests(metafunc):
         all_instance_generators = (
             ecole.instance.SetCoverGenerator(n_rows=100, n_cols=200),
             ecole.instance.CombinatorialAuctionGenerator(n_items=50, n_bids=150),
-            ecole.instance.CapacitatedFacilityLocationGenerator(n_customers=60, n_facilities=50),
             ecole.instance.IndependentSetGenerator(n_nodes=100),
         )
         metafunc.parametrize("instance_generator", all_instance_generators)
@@ -62,11 +61,17 @@ def test_repeated_slice_iteration(instance_generator):
 
 def test_SetCoverGenerator_parameters():
     """Parameters are bound in the constructor and as attributes."""
-    generator = I.SetCoverGenerator(n_cols=10)
+    generator = ecole.instance.SetCoverGenerator(n_cols=10)
     assert generator.n_cols == 10
 
 
 def test_IndependentSetGenerator_parameters():
     """Parameters are bound in the constructor and as attributes."""
-    generator = I.IndependentSetGenerator(graph_type="erdos_renyi")
+    generator = ecole.instance.IndependentSetGenerator(graph_type="erdos_renyi")
     assert generator.graph_type.name == "erdos_renyi"
+
+
+def test_CombinatorialAuctionGenerator_parameters():
+    """Parameters are bound in the constructor and as attributes."""
+    generator = ecole.instance.CombinatorialAuctionGenerator(additivity=-1)
+    assert generator.additivity == -1
