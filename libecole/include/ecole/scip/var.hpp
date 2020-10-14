42785f54ef3b876e0b12192dda11d00d5fee697b
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-10-14 17:48:23 -0400

    Add create_var_basic and create_cons_basic_linear

---
+++ libecole/include/ecole/scip/var.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <memory>
+
+#include <scip/scip.h>
+#include <scip/scip_var.h>
+
+#include "ecole/scip/utils.hpp"
+
+namespace ecole::scip {
+
+/**
+ * Create a variable with automatic management (RAII).
+ *
+ * The variable is returned in a unique_ptr that will automatically call SCIPreleaseVar on deletion.
+ * Even if the release is done automatically the SCIP semantics are not changed and the variable must not outlive the
+ * SCIP pointer (it is needed to release the variable).
+ *
+ * The arguments are forwarded to SCIPcreateVarBasic.
+ */
+template <typename... Args> auto create_var_basic(SCIP* scip, Args&&... args) {
+	SCIP_VAR* var = nullptr;
+	scip::call(SCIPcreateVarBasic, scip, &var, std::forward<Args>(args)...);
+	return std::unique_ptr{var, [scip](SCIP_VAR* ptr) { scip::call(SCIPreleaseVar, scip, &ptr); }};
+}
+
+}  // namespace ecole::scip
