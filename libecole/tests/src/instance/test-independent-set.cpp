5ee97739ad6b13709a6e418515d79e18b859fe91
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-10-26 17:49:36 -0400

    Strenthen instance generation unit tests

--- libecole/tests/src/instance/test-independent-set.cpp
+++ libecole/tests/src/instance/test-independent-set.cpp
@@ -11,5 +11,7 @@ using namespace ecole;
 TEST_CASE("IndependentSetGenerator unit test", "[unit][instance]") {
 	// Keep problem size reasonable for tests
 	std::size_t constexpr n_nodes = 100;
-	instance::unit_tests(instance::IndependentSetGenerator{{n_nodes}});
+	using Parameters = instance::IndependentSetGenerator::Parameters;
+	instance::unit_tests(instance::IndependentSetGenerator{{n_nodes, Parameters::erdos_renyi}});
+	instance::unit_tests(instance::IndependentSetGenerator{{n_nodes, Parameters::barabasi_albert}});
 }
