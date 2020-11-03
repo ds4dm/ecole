6c4abf13ad66c17823dfa89c5ecf9dda983d5314
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-11-03 11:59:24 -0500

    Add has_dereference trait

--- libecole/include/ecole/utility/type_traits.hpp
+++ libecole/include/ecole/utility/type_traits.hpp
@@ -12,4 +12,16 @@ namespace ecole::utility {
  */
 template <typename T> using value_or_const_ref_t = std::conditional_t<std::is_trivially_copyable_v<T>, T, T const&>;
 
+/**
+ * A trait to detect if a type has a dereference operator, that is behaves like a pointer.
+ *
+ * This is useful to write generic code where a function should have the same behaviour on a type or a pointer to that
+ * type.
+ */
+template <typename, typename = std::void_t<>> struct has_dereference : std::false_type {};
+
+template <typename T> struct has_dereference<T, std::void_t<decltype(*std::declval<T>())>> : std::true_type {};
+
+template <typename T> inline constexpr bool has_dereference_v = has_dereference<T>::value;
+
 }  // namespace ecole::utility
