e4504a9c4af1a4b5f3a8cda8a9b13825efe8a091
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-11-12 16:19:08 -0500

    Add unary reward

---
+++ libecole/include/ecole/reward/unary.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <utility>
+
+#include "ecole/reward/abstract.hpp"
+
+namespace ecole::reward {
+
+/** A class to map unary opertaion (such as exp(), log()...) onto rewards. */
+template <typename RewardFunc, typename UnaryFunc> class UnaryFunction : RewardFunction {
+public:
+	/** Default construct all functions. */
+	UnaryFunction() = default;
+
+	/** Store copies of the functions. */
+	UnaryFunction(RewardFunc reward_func, UnaryFunc unary_func) :
+		reward_function{std::move(reward_func)}, unary_function{std::move(unary_func)} {}
+
+	/** Call reset on the reward function. */
+	void reset(scip::Model& model) override { reward_function.reset(model); }
+
+	/** Extract reward and call the unary operation on it. */
+	Reward obtain_reward(scip::Model& model, bool done = false) override {
+		return unary_function(reward_function.obtain_reward(model, done));
+	}
+
+private:
+	RewardFunc reward_function;
+	UnaryFunc unary_function;
+};
+
+}  // namespace ecole::reward
