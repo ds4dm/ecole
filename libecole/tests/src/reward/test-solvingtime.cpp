56e70ab5491c429afbd7f7e46c0643249495f14d
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-11-20 16:18:33 -0500

    Fix test-solvingtime

--- libecole/tests/src/reward/test-solvingtime.cpp
+++ libecole/tests/src/reward/test-solvingtime.cpp
@@ -18,13 +18,13 @@ TEST_CASE("Solving time rewards are positive initially", "[reward]") {
 	auto model = get_model();  // a non-trivial instance is loaded
 
 	SECTION("Solving time is nonnegative before presolving") {
-		reward_func.reset(model);
-		REQUIRE(reward_func.obtain_reward(model) >= 0);
+		reward_func.before_reset(model);
+		REQUIRE(reward_func.extract(model) >= 0);
 	}
 
 	SECTION("Solving time is stricly positive after root node processing") {
-		reward_func.reset(model);
+		reward_func.before_reset(model);
 		model.solve_iter();  // presolve and stop at the root node before branching
-		REQUIRE(reward_func.obtain_reward(model) > 0);
+		REQUIRE(reward_func.extract(model) > 0);
 	}
 }
