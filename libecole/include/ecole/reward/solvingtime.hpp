a15a755351064859eb1bc950b84661563d42cdfd
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-11-20 15:50:06 -0500

    Small C++ improvement in solvingtime

--- libecole/include/ecole/reward/solvingtime.hpp
+++ libecole/include/ecole/reward/solvingtime.hpp
@@ -1,13 +1,13 @@
 #pragma once
 
 #include "ecole/reward/abstract.hpp"
-#include "ecole/scip/type.hpp"
 
 namespace ecole::reward {
 
 class SolvingTime : public RewardFunction {
 public:
-	SolvingTime(bool wall_ = false) : wall(wall_) {}
+	SolvingTime(bool wall_ = false) noexcept : wall(wall_) {}
+
 	void before_reset(scip::Model& model) override;
 	Reward extract(scip::Model& model, bool done = false) override;
 
