eb450cc1ef48651f31935df8cf3c6ca7baf580e0
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-11-20 15:44:53 -0500

    Remove solving time integration test

--- libecole/tests/src/reward/test-solvingtime.cpp
+++ libecole/tests/src/reward/test-solvingtime.cpp
@@ -1,7 +1,5 @@
 #include <catch2/catch.hpp>
 
-#include "ecole/environment/branching.hpp"
-#include "ecole/observation/nothing.hpp"
 #include "ecole/reward/solvingtime.hpp"
 
 #include "conftest.hpp"
@@ -10,12 +8,12 @@
 using namespace ecole;
 
 TEST_CASE("SolvingTime unit tests", "[unit][reward]") {
-	bool wall = GENERATE(true, false);
+	bool const wall = GENERATE(true, false);
 	reward::unit_tests(reward::SolvingTime{wall});
 }
 
 TEST_CASE("Solving time rewards are positive initially", "[reward]") {
-	bool wall = GENERATE(true, false);
+	bool const wall = GENERATE(true, false);
 	auto reward_func = reward::SolvingTime{wall};
 	auto model = get_model();  // a non-trivial instance is loaded
 
@@ -28,26 +26,5 @@ TEST_CASE("Solving time rewards are positive initially", "[reward]") {
 		reward_func.reset(model);
 		model.solve_iter();  // presolve and stop at the root node before branching
 		REQUIRE(reward_func.obtain_reward(model) > 0);
-	}
-}
-
-TEST_CASE("Solving time rewards are always strictly positive when used in a Branching environment", "[reward]") {
-	constexpr int max_nnodes = 20;
-	bool wall = GENERATE(true, false);
-	auto env = environment::Branching<observation::Nothing, reward::SolvingTime>{
-		{},
-		{reward::SolvingTime{wall}},
-		{
-			{"presolving/maxrounds", 0},  // just to save time here
-			{"limits/totalnodes", max_nnodes},
-		},
-		false};
-
-	auto [obs, action_set, reward, done] = env.reset(problem_file);
-	REQUIRE(reward > 0);
-
-	while (!done) {
-		std::tie(std::ignore, action_set, reward, done, std::ignore) = env.step(action_set.value()[0]);  // dumb action
-		REQUIRE(reward >= 0);
 	}
 }
