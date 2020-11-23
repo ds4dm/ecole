dcae8ecc5188c043267de79639ef1385b356d788
jdumouchelle <jdumouch@uwaterloo.ca>
2020-11-23 11:10:58 -0500

    initial commit of set-cover and cauction cpp generators

--- libecole/src/instance/combinatorial-auction.cpp
+++ libecole/src/instance/combinatorial-auction.cpp
@@ -1,12 +1,23 @@
+#include <fmt/format.h>
+#include <map>
+#include <xtensor/xadapt.hpp>
+#include <xtensor/xindex_view.hpp>
+#include <xtensor/xrandom.hpp>
+#include <xtensor/xsort.hpp>
+#include <xtensor/xtensor.hpp>
+#include <xtensor/xview.hpp>
+
 #include "ecole/instance/combinatorial-auction.hpp"
+#include "ecole/scip/cons.hpp"
+#include "ecole/scip/model.hpp"
+#include "ecole/scip/utils.hpp"
+#include "ecole/scip/var.hpp"
 
 namespace ecole::instance {
 
-scip::Model
-CombinatorialAuctionGenerator::generate_instance(RandomEngine& /*random_engine*/, Parameters /*parameters*/) {
-	// TODO implement algorithm
-	return {};
-}
+/**************************************************
+ *  CombinatorialAuctionGenerator methods  *
+ **************************************************/
 
 CombinatorialAuctionGenerator::CombinatorialAuctionGenerator(RandomEngine random_engine_, Parameters parameters_) :
 	random_engine{random_engine_}, parameters{parameters_} {}
@@ -20,6 +31,314 @@ scip::Model CombinatorialAuctionGenerator::next() {
 
 void CombinatorialAuctionGenerator::seed(Seed seed) {
 	random_engine.seed(seed);
+}
+
+namespace {
+
+using std::size_t;
+using xvector = xt::xtensor<double, 1>;
+using xvector_size_t = xt::xtensor<size_t, 1>;
+using xmatrix = xt::xtensor<double, 2>;
+
+/** Sample with replacement based on weights given by a probability or weight vector.
+ *
+ * Samples n_samples values from a weighted distribution defined by the weights.
+ * The values are in the range of [1, weights.size()].
+ */
+auto arg_choice_without_replacement(size_t n_samples, xvector weights, RandomEngine& random_engine) -> xvector_size_t {
+
+	auto const n_items = weights.size();
+
+	auto weight_sum = xt::sum(weights)();
+	xvector weights_cumsum = xt::cumsum(weights);
+
+	xvector samples = xt::random::rand({n_samples}, 0.0, weight_sum, random_engine);
+	xvector_size_t indices({n_samples});
+
+	for (size_t i = 0; i < n_samples; ++i) {
+		size_t index = n_items - 1;
+		for (size_t j = 0; j < n_items - 1; ++j) {
+			if (samples[i] >= weights_cumsum[j] && samples[i] <= weights_cumsum[j + 1]) {
+				index = j;
+			}
+		}
+		indices[i] = index;
+	}
+	return indices;
+}
+
+/** Choose the next item to be added to the bundle/sub-bundle.
+ *
+ */
+auto choose_next_item(
+	xt::xtensor<int, 1> bundle_mask,
+	xvector interests,
+	xmatrix compats,
+	RandomEngine& random_engine) {
+	auto compats_masked = xt::index_view(compats, bundle_mask);
+	auto compats_masked_mean = xt::sum(compats_masked, 0);
+	xvector probs = (1 - bundle_mask) * interests * compats_masked_mean;
+	probs = probs / xt::sum(probs);
+	return arg_choice_without_replacement(1, probs, random_engine)(0);
+}
+
+/** Adds a single variable with the coefficient price
+ *
+ */
+auto add_var(SCIP* scip, size_t i, double price) {
+	auto const name = fmt::format("x_{}", i);
+	auto unique_var = scip::create_var_basic(scip, name.c_str(), 0., 1., price, SCIP_VARTYPE_BINARY);
+	auto* var_ptr = unique_var.get();
+	scip::call(SCIPaddVar, scip, var_ptr);
+	return var_ptr;
+}
+
+/** Adds all constraints to the SCIP model.
+ *
+ */
+auto add_constraints(SCIP* scip, xt::xtensor<SCIP_VAR*, 1> vars, std::vector<std::vector<size_t>> bids_per_item) {
+
+	for (size_t i = 0; i < bids_per_item.size(); ++i) {
+
+		std::vector<size_t> item_bids = bids_per_item[i];
+
+		if (item_bids.size() > 0) {
+			auto cons_vars = xt::xtensor<SCIP_VAR*, 1>{{item_bids.size()}};
+			auto coefs = xt::xtensor<scip::real, 1>(cons_vars.shape(), 1.);
+			for (size_t j = 0; j < item_bids.size(); ++j) {
+				cons_vars(j) = vars(item_bids[j]);
+			}
+			auto name = fmt::format("c_{}", i);
+			auto const inf = SCIPinfinity(scip);
+			auto cons =
+				scip::create_cons_basic_linear(scip, name.c_str(), cons_vars.size(), &cons_vars(0), coefs.data(), -inf, 1.0);
+			scip::call(SCIPaddCons, scip, cons.get());
+		}
+	}
+}
+
+}  // namespace
+
+/*************************************************************
+ *  CombinatorialAuctionGenerator::generate_instance  *
+ *************************************************************/
+
+scip::Model CombinatorialAuctionGenerator::generate_instance(RandomEngine& random_engine, Parameters parameters) {
+
+	// TODO: implement logging.
+	bool log = false;
+
+	assert(parameters.min_value >= 0 && parameters.max_value >= parameters.min_value);
+	assert(parameters.add_item_prob >= 0 && parameters.add_item_prob <= 1);
+
+	// get values
+	xvector rand_val = xt::random::rand({parameters.n_items}, 0.0, 1.0, random_engine);
+	xvector values = parameters.min_value + (parameters.max_value - parameters.min_value) * rand_val;
+
+	// get compatibilities
+	xmatrix compats_rand = xt::random::rand({parameters.n_items, parameters.n_items}, 0.0, 1.0, random_engine);
+	xmatrix compats = xt::triu(compats_rand, 1);
+	xmatrix compats_T = xt::transpose(compats);
+	compats = compats + compats_T;
+	compats = compats / xt::sum(compats, 1);
+
+	size_t n_dummy_items = 0;
+	size_t bid_index = 0;
+
+	std::vector<std::vector<size_t>> bids_bundle;
+	std::vector<double> bids_price;
+
+	while (bid_index < parameters.n_bids) {
+
+		// bidder item values (buy price) and interests
+		xvector private_interests = xt::random::rand({parameters.n_items}, 0.0, 1.0, random_engine);
+		xvector private_values = values + parameters.max_value * parameters.value_deviation * (2 * private_interests - 1);
+
+		// substitutable bids of this bidder
+		std::map<std::vector<size_t>, double> bidder_bids = {};
+
+		// generate initial bundle, choose first item according to bidder interests
+		xvector probs = private_interests / xt::sum(private_interests);
+		size_t item = arg_choice_without_replacement(1, probs, random_engine)(0);
+
+		xt::xtensor<int, 1> bundle_mask = xt::zeros<int>({parameters.n_items});
+		bundle_mask(item) = 1;
+
+		// add additional items, according to bidder interests and item compatibilities
+		while (true) {
+			double sampled_prob = xt::random::rand({1}, 0.0, 1.0, random_engine)[0];
+			if (sampled_prob > parameters.add_item_prob) {
+				break;
+			}
+
+			size_t mask_sum = static_cast<size_t>(xt::sum(bundle_mask)());
+			if (mask_sum == parameters.n_items) {
+				break;
+			}
+
+			item = choose_next_item(bundle_mask, private_interests, compats, random_engine);
+			bundle_mask(item) = 1;
+		}
+
+		auto bundle = xt::nonzero(bundle_mask)[0];
+
+		// get price of bundle
+		xvector private_values_slice = xt::index_view(private_values, bundle);
+		double private_value_slice_sum = xt::sum(private_values_slice)();
+		double bundle_power = std::pow(static_cast<double>(bundle.size()), 1.0 + parameters.additivity);
+		double price = private_value_slice_sum + bundle_power;
+
+		if (parameters.integers) {
+			price = floor(price);
+		}
+
+		if (log && price < 0) {
+			// TODO: Logger implementation
+			std::cout << "log: price < 0 **" << std::endl;
+		}
+
+		std::vector<size_t> bundle_vector(bundle.begin(), bundle.end());
+		bidder_bids[bundle_vector] = price;
+
+		// get sub-bundles
+		std::vector<std::vector<size_t>> sub_candidates_bundle;
+		std::vector<double> sub_candidates_price;
+
+		for (size_t i = 0; i < bundle.size(); ++i) {
+
+			size_t sub_item = bundle[i];
+
+			// at least one item must be shared with initial bundle
+			xt::xtensor<int, 1> sub_bundle_mask = xt::zeros<int>({parameters.n_items});
+			sub_bundle_mask(sub_item) = 1;
+
+			// add additional items, according to bidder interests and item compatibilities
+			while (true) {
+				size_t sub_mask_sum = static_cast<size_t>(xt::sum(sub_bundle_mask)());
+				if (sub_mask_sum > bundle.size()) {
+					break;
+				}
+
+				sub_item = choose_next_item(sub_bundle_mask, private_interests, compats, random_engine);
+				sub_bundle_mask(sub_item) = 1;
+			}
+
+			auto sub_bundle = xt::nonzero(sub_bundle_mask)[0];
+			std::vector<size_t> sub_bundle_vector(sub_bundle.begin(), sub_bundle.end());
+
+			// get price of sub-bundle
+			xvector sub_private_values_slice = xt::index_view(private_values, sub_bundle);
+			double sub_private_value_slice_sum = xt::sum(sub_private_values_slice)();
+			double sub_bundle_power = std::pow(static_cast<double>(sub_bundle.size()), 1.0 + parameters.additivity);
+			double sub_price = sub_private_value_slice_sum + sub_bundle_power;
+
+			if (parameters.integers) {
+				sub_price = floor(sub_price);
+			}
+
+			sub_candidates_bundle.push_back(sub_bundle_vector);
+			sub_candidates_price.push_back(sub_price);
+		}
+
+		//
+		double budget = parameters.budget_factor * price;
+
+		xvector value_slice = xt::index_view(values, bundle);
+		double value_slice_sum = xt::sum(value_slice)();
+		double min_resale_value = parameters.resale_factor * value_slice_sum;
+
+		xvector price_tensor = xt::adapt(sub_candidates_price, {sub_candidates_price.size()});
+		xvector_size_t sorted_indices = xt::argsort(price_tensor);
+
+		for (size_t i = 0; i < sorted_indices.size(); ++i) {
+			auto bundle_i = sub_candidates_bundle[sorted_indices(i)];
+			auto price_i = sub_candidates_price[sorted_indices(i)];
+
+			if (bidder_bids.size() >= parameters.max_n_sub_bids + 1 || bid_index + bidder_bids.size() >= parameters.n_bids) {
+				break;
+			}
+
+			if (log && price_i < 0) {
+				// TODO: Logger implementation
+				std::cout << "log: price < 0" << std::endl;
+				continue;
+			}
+
+			if (log && price_i > budget) {
+				// TODO: Logger implementation
+				std::cout << "log: price > budget" << std::endl;
+				continue;
+			}
+
+			xvector value_slice_i = xt::index_view(values, bundle_i);
+			double value_slice_sum_i = xt::sum(value_slice)();
+			if (log && value_slice_sum_i < min_resale_value) {
+				// TODO: Logger implementation
+				std::cout << "log: sum < min_resale_value" << std::endl;
+				continue;
+			}
+
+			std::vector<size_t> bundle_vector_i(bundle_i.begin(), bundle_i.end());
+			if (log && bidder_bids.count(bundle_vector_i)) {
+				// TODO: Logger implementation
+				std::cout << "log: duplicate bid" << std::endl;
+				continue;
+			}
+
+			bidder_bids[bundle_vector_i] = price_i;
+		}
+
+		size_t dummy_item = 0;
+		if (bidder_bids.size() > 2) {
+			dummy_item = parameters.n_items + n_dummy_items;
+			++n_dummy_items;
+		}
+
+		// add bids
+		std::map<std::vector<size_t>, double>::iterator it;
+		for (it = bidder_bids.begin(); it != bidder_bids.end(); ++it) {
+			std::vector<size_t> bund = it->first;
+			if (dummy_item) {
+				bund.push_back(dummy_item);
+			}
+			bids_bundle.push_back(bund);
+			bids_price.push_back(it->second);
+			++bid_index;
+		}
+
+	}  // loop to get bids
+
+	// create scip model
+	auto model = scip::Model::prob_basic();
+	auto* const scip = model.get_scip_ptr();
+	scip::call(SCIPsetObjsense, scip, SCIP_OBJSENSE_MAXIMIZE);
+
+	//  initialize bids_per_item vector
+	std::vector<std::vector<size_t>> bids_per_item;
+	for (size_t i = 0; i < parameters.n_items + n_dummy_items; ++i) {
+		std::vector<size_t> bids_per_item_i;
+		bids_per_item.push_back(bids_per_item_i);
+	}
+
+	// add variables
+	auto vars = xt::xtensor<SCIP_VAR*, 1>{{bids_bundle.size()}};
+	for (size_t i = 0; i < bids_bundle.size(); ++i) {
+		std::vector<size_t> bundle_i = bids_bundle[i];
+		double price_i = bids_price[i];
+
+		// add variable to pyscipopt
+		vars(i) = add_var(scip, i, price_i);
+
+		// add to bids_per_item
+		for (size_t j = 0; j < bundle_i.size(); ++j) {
+			bids_per_item[bundle_i[j]].push_back(i);
+		}
+	}
+
+	// add constraints
+	add_constraints(scip, vars, bids_per_item);
+
+	return model;
 }
 
 }  // namespace ecole::instance
--- libecole/src/instance/set-cover.cpp
+++ libecole/src/instance/set-cover.cpp
@@ -1,11 +1,21 @@
+#include <fmt/format.h>
+#include <map>
+#include <xtensor/xrandom.hpp>
+#include <xtensor/xsort.hpp>
+#include <xtensor/xtensor.hpp>
+#include <xtensor/xview.hpp>
+
 #include "ecole/instance/set-cover.hpp"
+#include "ecole/scip/cons.hpp"
+#include "ecole/scip/model.hpp"
+#include "ecole/scip/utils.hpp"
+#include "ecole/scip/var.hpp"
 
 namespace ecole::instance {
 
-scip::Model SetCoverGenerator::generate_instance(RandomEngine& /*random_engine*/, Parameters /*parameters*/) {
-	// TODO implement algorithm
-	return {};
-}
+/**************************************************
+ *  SetCoverGenerator methods  *
+ **************************************************/
 
 SetCoverGenerator::SetCoverGenerator(RandomEngine random_engine_, Parameters parameters_) :
 	random_engine{random_engine_}, parameters{parameters_} {}
@@ -20,5 +30,247 @@ scip::Model SetCoverGenerator::next() {
 void SetCoverGenerator::seed(Seed seed) {
 	random_engine.seed(seed);
 }
+
+namespace {
+
+using std::size_t;
+using xvector = xt::xtensor<size_t, 1>;
+
+/** Sets a slice of a 1-D xtensor.
+ *
+ * Sets the the slice from start_index to end_index of vector a to
+ * vector b.  Vector b should be of size (end_index - start_index).
+ */
+auto set_slice(xvector& a, xvector& b, size_t start_index, size_t end_index) {
+	for (size_t i = 0; i < end_index - start_index; ++i) {
+		a(start_index + i) = b(i);
+	}
+}
+
+/** Gets a slice of a 1-D xtensor.
+ *
+ * Gets the the slice from start_index to end_index of vector a.
+ */
+auto get_slice(xvector& a, size_t start_index, size_t end_index) -> xvector {
+	xvector slice({end_index - start_index}, 0);
+	for (size_t i = 0; i < end_index - start_index; ++i) {
+		slice(i) = a(i + start_index);
+	}
+	return slice;
+}
+
+/** Gets a 1-D xtensor in a specified range.
+ *
+ * Gets the 1-D xtensor from start to end
+ * incremeneted by incremenet.
+ */
+auto get_range(size_t start, size_t end, size_t increment) -> xvector {
+	xvector range = xt::arange<size_t>(start, end, increment);
+	return range;
+}
+
+/** Gets the counts of each element in a 1-D xtensor.
+ *
+ * This method is specifically designed in the case where
+ * we know each element is in the range [0, n_cols].
+ */
+auto get_counts(xvector& indices) -> xvector {
+
+	// get count for each unique element
+	std::map<size_t, size_t> unique_count_map;
+	for (size_t i = 0; i < indices.size(); ++i) {
+		if (!unique_count_map.count(indices(i))) {
+			unique_count_map[indices(i)] = 1;
+		} else {
+			++unique_count_map[indices(i)];
+		}
+	}
+
+	xvector counts({unique_count_map.size()}, 0);
+	for (size_t i = 0; i < unique_count_map.size(); ++i) {
+		counts(i) = unique_count_map[i];
+	}
+
+	return counts;
+}
+
+/** Samples values in a range and returns them as a 1-D xtensor.
+ *
+ * Samples num_samples values in the range from start_index to
+ * end_index.
+ */
+auto get_choice_in_range(size_t start_index, size_t end_index, size_t num_samples, RandomEngine& random_engine) -> xvector {
+	xvector choices = get_range(start_index, end_index, 1);
+	xvector samples = xt::random::choice(choices, num_samples, false, random_engine);
+	return samples;
+}
+
+/** Adds a varaible to the SCIP Model.
+ *
+ * Adds a single binary variable with a specified cost.
+ */
+auto add_var(SCIP* scip, size_t idx, scip::real cost) -> SCIP_VAR* {
+	auto const name = fmt::format("x_{}", idx);
+	auto unique_var = scip::create_var_basic(scip, name.c_str(), 0., 1., cost, SCIP_VARTYPE_BINARY);
+	auto* var_ptr = unique_var.get();
+	scip::call(SCIPaddVar, scip, var_ptr);
+	return var_ptr;
+}
+
+/** Adds all varaibles to the SCIP Model.
+ *
+ * A variable is added for each element (or column) in the
+ * set cover problem.
+ */
+auto add_vars(SCIP* scip, xt::xtensor<scip::real, 1> const& c) -> xt::xtensor<SCIP_VAR*, 1> {
+	auto vars = xt::xtensor<SCIP_VAR*, 1>{{c.shape()}};
+	for (size_t i = 0; i < c.size(); ++i) {
+		vars(i) = add_var(scip, i, c(i));
+	}
+	return vars;
+}
+
+/** Adds set covering constaints.
+ *
+ * For each set, at least one element is required to the solution.
+ */
+auto add_constaints(
+	SCIP* scip, 
+	xt::xtensor<SCIP_VAR*, 1> vars,
+	xvector& indices,
+	xvector& indptr,
+	size_t n_rows) {
+
+	for (size_t i = 0; i < n_rows; ++i) {
+
+		// get the set of variables to be added in the constraint.
+		auto cons_vars = xt::xtensor<SCIP_VAR*, 1>{{indptr(i + 1) - indptr(i)}};
+		for (size_t j = indptr(i); j < indptr(i + 1); ++j) {
+			cons_vars(j - indptr(i)) = vars(indices(j));
+		}
+
+		// add constraint to SCIP Model.
+		auto name = fmt::format("c_{}", i);
+		auto const inf = SCIPinfinity(scip);
+		auto coefs = xt::xtensor<scip::real, 1>(cons_vars.shape(), 1.);
+		auto cons =
+			scip::create_cons_basic_linear(scip, name.c_str(), cons_vars.size(), &cons_vars(0), coefs.data(), 1.0, inf);
+		scip::call(SCIPaddCons, scip, cons.get());
+	}
+}
+
+/** Convert CSC sparse indicies and index pointers to CSR.
+ *
+ * This implementation only converts the indices and points,
+ * and does not consider the values as they are all 1.  A
+ * tuple of the indices and index pointers in CSR format are
+ * returned.
+ */
+auto convert_csc_to_csr(
+	xvector& indices, 
+	xvector& indptr, 
+	size_t n_rows, 
+	size_t n_cols, 
+	size_t nnzrs, 
+	xvector& indptr_csr, 
+	xvector& indices_csr) {
+
+	for (size_t j = 0; j < nnzrs; ++j) {
+		indptr_csr(indices(j) + 1) += 1;
+	}
+	indptr_csr = xt::cumsum(indptr_csr);
+
+	for (size_t col = 0; col < n_cols; ++col) {
+		for (size_t jj = indptr(col); jj < indptr(col + 1); ++jj) {
+			size_t row = indices(jj);
+			indices_csr(indptr_csr(row)) = col;
+			indptr_csr(row) = indptr_csr(row) + 1;
+		}
+	}
+
+	size_t last = 0;
+	for (size_t row = 0; row <= n_rows; ++row) {
+		size_t temp = indptr_csr[row];
+		indptr_csr(row) = last;
+		last = temp;
+	}
+	return;
+}
+
+}  // namespace
+
+/*************************************************************
+ *  SetCoverGenerator::generate_instance  *
+ *************************************************************/
+
+scip::Model SetCoverGenerator::generate_instance(RandomEngine& random_engine, Parameters parameters) {
+
+	auto n_rows = static_cast<size_t>(parameters.n_rows);
+	auto n_cols = static_cast<size_t>(parameters.n_cols);
+	auto density = parameters.density;
+	auto max_coef = static_cast<size_t>(parameters.max_coef);
+
+	auto nnzrs = static_cast<size_t>(static_cast<double>(n_rows * n_cols) * density);
+
+	xvector indices({nnzrs}, 0);
+
+	// force at least 2 rows per col
+	xvector first_indices = get_range(0, 2 * n_cols, 1) % n_cols;
+	set_slice(indices, first_indices, 0, 2 * n_cols);
+
+	// assign remaining column indexes at random
+	xvector samples = get_choice_in_range(0, n_cols * (n_rows - 2), nnzrs - (2 * n_cols), random_engine) % n_cols;
+	set_slice(indices, samples, 2 * n_cols, nnzrs);
+
+	// get counts of unique elements
+	xvector col_n_rows = get_counts(indices);
+
+	// ensure at least 1 column per row
+	xvector perm = xt::random::permutation<size_t>(n_rows, random_engine);
+	set_slice(indices, perm, 0, n_rows);
+
+	size_t i = 0;
+	xvector indptr({n_cols + 1}, 0);
+	size_t indptr_idx = 1;
+
+	for (size_t idx = 0; idx < col_n_rows.size(); ++idx) {
+		size_t n = col_n_rows(idx);
+
+		if (i + n <= n_rows) {
+		} else if (i >= n_rows) {
+			xvector samples1 = get_choice_in_range(0, n_rows, n, random_engine);
+			set_slice(indices, samples1, i, i + n);
+
+		} else if (i + n > n_rows) {
+			xvector remaining_rows = xt::setdiff1d(get_range(0, n_rows, 1), get_slice(indices, i, n_rows));
+			xvector choices_ = xt::random::choice(remaining_rows, i + n - n_rows, false, random_engine);
+			set_slice(indices, choices_, n_rows, i + n);
+		}
+
+		i += n;
+		indptr(indptr_idx) = i;
+		++indptr_idx;
+	}
+
+	// convert csc indices/ptrs to csr
+	xvector indptr_csr({n_rows + 1}, 0);
+	xvector indices_csr({nnzrs}, 0);
+	convert_csc_to_csr(indices, indptr, n_rows, n_cols, nnzrs, indptr_csr, indices_csr);
+
+	// sample coefficients
+	xt::xtensor<scip::real, 1> c = xt::random::randint<size_t>({n_cols}, 0, max_coef, random_engine);
+
+	// create scip model
+	auto model = scip::Model::prob_basic();
+	auto* const scip = model.get_scip_ptr();
+	scip::call(SCIPsetObjsense, scip, SCIP_OBJSENSE_MINIMIZE);
+
+	// add variables and constraints
+	auto const vars = add_vars(scip, c);
+	add_constaints(scip, vars, indices_csr, indptr_csr, n_rows);
+
+	return model;
+
+}  // generate_instance
 
 }  // namespace ecole::instance
