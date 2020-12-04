4b8fe2aab1e70820c85f6eb4369317fc6f9ad989
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-12-04 14:44:53 +0100

    Rename reset > before_reset, obatain_observation > extract

--- libecole/tests/src/observation/test-khalil-2016.cpp
+++ libecole/tests/src/observation/test-khalil-2016.cpp
@@ -28,8 +28,8 @@ TEST_CASE("Khalil2016 return correct observation", "[obs]") {
 
 	auto obs_func = observation::Khalil2016{};
 	auto model = get_solving_model();
-	obs_func.reset(model);
-	auto const optional_obs = obs_func.obtain_observation(model);
+	obs_func.before_reset(model);
+	auto const optional_obs = obs_func.extract(model, false);
 
 	SECTION("Observation is not empty on non terminal state") { REQUIRE(optional_obs.has_value()); }
 
