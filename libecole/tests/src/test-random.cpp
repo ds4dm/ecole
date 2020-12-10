ec96a8377ee7e66b58d5ea5870597eeb4bee64b9
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-12-10 14:38:15 +0100

    Implement RandomEngine serialization

--- libecole/tests/src/test-random.cpp
+++ libecole/tests/src/test-random.cpp
@@ -25,3 +25,10 @@ TEST_CASE("Successive random engines are different", "[random]") {
 	auto random_engine_2 = ecole::spawn_random_engine();
 	REQUIRE(random_engine_1 != random_engine_2);
 }
+
+TEST_CASE("Random engine serialization", "[random]") {
+	auto const engine = RandomEngine{42};  // NOLINT This is deterministic for the test
+	auto const data = serialize(engine);
+	auto const engine_copy = deserialize(data);
+	REQUIRE(engine == engine_copy);
+}
