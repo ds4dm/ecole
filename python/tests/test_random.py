d308b2ce0ec161940e365896865279ab41959eee
AntoinePrv <AntoinePrv@users.noreply.github.com>
2020-12-10 14:54:04 +0100

    Add copy and pickle support for RandomEngine

--- python/tests/test_random.py
+++ python/tests/test_random.py
@@ -1,3 +1,6 @@
+import copy
+import pickle
+
 import ecole
 
 
@@ -11,6 +14,25 @@ def test_RandomEngine():
     random_engine.seed(42)
     rand_val_2 = random_engine()
     assert rand_val_1 == rand_val_2
+
+
+def test_RandomEngine_copy():
+    """Copy create a new RandomEngine with same state."""
+    random_engine = ecole.RandomEngine(42)
+
+    random_engine_copy = copy.copy(random_engine)
+    assert random_engine_copy == random_engine
+    assert random_engine_copy is not random_engine
+
+    random_engine_deepcopy = copy.deepcopy(random_engine)
+    assert random_engine_deepcopy == random_engine
+    assert random_engine_deepcopy is not random_engine
+
+
+def test_RandomEngine_pickle():
+    """Pickle preserve the state of the RandomEngine."""
+    random_engine = ecole.RandomEngine(42)
+    assert random_engine == pickle.loads(pickle.dumps(random_engine))
 
 
 def test_same_seed():
