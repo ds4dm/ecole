507b78ef255b24e6f4f81dd22c464f4b89ee2bbf
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-01-18 15:20:40 -0500

    Add CPU clock

---
+++ libecole/include/ecole/utility/chrono.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <chrono>
+
+namespace ecole::utility {
+
+/**
+ * A CPU usage clock.
+ *
+ * Measure time the CPU spent processing the program’s instructions.
+ * This count both the System (kernel) and User CPU time.
+ * The time spent waiting for other things to complete (like I/O operations) is not included in the CPU time.
+ *
+ * The implementation uses OS dependent functionality.
+ */
+class cpu_clock {
+public:
+	using duration = std::chrono::nanoseconds;
+	using rep = duration::rep;
+	using period = duration::period;
+	using time_point = std::chrono::time_point<cpu_clock>;
+	static bool constexpr is_steady = true;
+
+	static auto now() -> time_point;
+};
+
+}  // namespace ecole::utility
