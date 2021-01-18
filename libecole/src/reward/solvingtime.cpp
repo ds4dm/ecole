aea1f5651918067a417fb9feaefac0b77258274d
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-01-18 15:48:53 -0500

    Refactor solvingtime to use cpu_clock

--- libecole/src/reward/solvingtime.cpp
+++ libecole/src/reward/solvingtime.cpp
@@ -1,39 +1,28 @@
 #include <chrono>
-#include <ctime>
 
 #include "ecole/reward/solvingtime.hpp"
+#include "ecole/utility/chrono.hpp"
 
 namespace ecole::reward {
 
 namespace {
 
-auto wall_clock() {
-	return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch())
-		.count();
+auto time_now(bool wall) -> std::chrono::nanoseconds {
+	if (wall) {
+		return std::chrono::steady_clock::now().time_since_epoch();
+	}
+	return utility::cpu_clock::now().time_since_epoch();
 }
 
 }  // namespace
 
 void SolvingTime::before_reset(scip::Model& /* model */) {
-	if (wall) {
-		solving_time_offset = wall_clock();
-	} else {
-		solving_time_offset = static_cast<long>(std::clock());
-	}
+	solving_time_offset = time_now(wall);
 }
 
 Reward SolvingTime::extract(scip::Model& /* model */, bool /* done */) {
-	double solving_time_diff = 0.0;
-	long now = 0;
-
-	if (wall) {
-		static auto constexpr mus_per_seconds = 1000 * 1000;
-		now = wall_clock();
-		solving_time_diff = static_cast<double>(now - solving_time_offset) / mus_per_seconds;
-	} else {
-		now = static_cast<long>(std::clock());
-		solving_time_diff = static_cast<double>(now - solving_time_offset) / CLOCKS_PER_SEC;
-	}
+	auto const now = time_now(wall);
+	auto const solving_time_diff = static_cast<double>((now - solving_time_offset).count());
 	solving_time_offset = now;
 	return solving_time_diff;
 }
