3fac98c6ee68cd4d138b7d08c77b57410512ab0d
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-01-18 15:48:53 -0500

    Refactor solvingtime to use cpu_clock

--- libecole/include/ecole/reward/solvingtime.hpp
+++ libecole/include/ecole/reward/solvingtime.hpp
@@ -1,19 +1,21 @@
 #pragma once
 
+#include <chrono>
+
 #include "ecole/reward/abstract.hpp"
 
 namespace ecole::reward {
 
 class SolvingTime : public RewardFunction {
 public:
-	SolvingTime(bool wall_ = false) noexcept : wall(wall_) {}
+	SolvingTime(bool wall_ = false) noexcept : wall{wall_} {}
 
 	void before_reset(scip::Model& model) override;
 	Reward extract(scip::Model& model, bool done = false) override;
 
 private:
 	bool wall = false;
-	long solving_time_offset = 0;
+	std::chrono::nanoseconds solving_time_offset;
 };
 
 }  // namespace ecole::reward
