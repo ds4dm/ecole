4611b5afd7480e23bc6457162c9b9f162206049f
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-01-18 15:20:40 -0500

    Add CPU clock

---
+++ libecole/tests/src/utility/test-chrono.cpp
@@ -0,0 +1,11 @@
+#include <catch2/catch.hpp>
+
+#include "ecole/utility/chrono.hpp"
+
+using namespace ecole;
+
+TEST_CASE("cpu_clock is monotonic", "[utility]") {
+	auto const before = utility::cpu_clock::now();
+	auto const after = utility::cpu_clock::now();
+	REQUIRE(before < after);
+}
