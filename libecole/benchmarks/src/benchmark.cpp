554bbc0a9375d91d2effdeb1a8fbbd83b83ae249
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-01-26 15:40:45 -0500

    Add benchmark run function

--- libecole/benchmarks/src/benchmark.cpp
+++ libecole/benchmarks/src/benchmark.cpp
@@ -1,3 +1,8 @@
+#include <algorithm>
+#include <functional>
+#include <iterator>
+#include <utility>
+
 #include "ecole/scip/model.hpp"
 
 #include "benchmark.hpp"
@@ -6,6 +11,25 @@ namespace ecole::benchmark {
 
 auto Instance::from_model(scip::Model const& model) -> Instance {
 	return {model.name(), model.variables().size(), model.constraints().size(), model.nnz()};
+}
+
+auto run(std::map<std::string, Competitor> const& competitors, scip::Model model) -> Run {
+	auto run = Run{Instance::from_model(model), {}};
+	for (auto const& [name, competitor] : competitors) {
+		run.metrics[name] = std::invoke(competitor, model.copy_orig());
+	}
+	return run;
+}
+
+auto run(std::map<std::string, Competitor> const& competitors, std::vector<scip::Model> models) -> std::vector<Run> {
+	auto runs = std::vector<Run>{};
+	runs.reserve(models.size());
+	std::transform(
+		std::make_move_iterator(models.begin()),
+		std::make_move_iterator(models.end()),
+		std::back_inserter(runs),
+		[&competitors](scip::Model&& model) { return run(competitors, std::move(model)); });
+	return runs;
 }
 
 }  // namespace ecole::benchmark
