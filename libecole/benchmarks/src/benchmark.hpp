eff3f96a96c68cb1b86030424b3123b37aab60ae
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-01-26 16:09:47 -0500

    Add Json to benchmarking

--- libecole/benchmarks/src/benchmark.hpp
+++ libecole/benchmarks/src/benchmark.hpp
@@ -5,6 +5,8 @@
 #include <type_traits>
 #include <vector>
 
+#include <nlohmann/json.hpp>
+
 #include "ecole/scip/model.hpp"
 
 namespace ecole::benchmark {
@@ -16,6 +18,8 @@ struct Instance {
 	std::size_t n_nonzero = 0;
 
 	static auto from_model(scip::Model const& model) -> Instance;
+
+	NLOHMANN_DEFINE_TYPE_INTRUSIVE(Instance, name, n_vars, n_cons, n_nonzero);
 };
 
 struct Metrics {
@@ -23,11 +27,15 @@ struct Metrics {
 	double cpu_time_s = 0.;
 	std::size_t n_nodes = 0;
 	std::size_t n_lp_iterations = 0;
+
+	NLOHMANN_DEFINE_TYPE_INTRUSIVE(Metrics, wall_time_s, cpu_time_s, n_nodes, n_lp_iterations);
 };
 
 struct Run {
 	Instance instance;
 	std::map<std::string, Metrics> metrics;
+
+	NLOHMANN_DEFINE_TYPE_INTRUSIVE(Run, instance, metrics);
 };
 
 using Competitor = std::add_pointer_t<Metrics(scip::Model)>;
