cb65391b2b80b80579c3944ad23762f359d78a56
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-01-28 17:32:28 -0500

    Change benchmark vector > generator

--- libecole/benchmarks/src/bench-branching.hpp
+++ libecole/benchmarks/src/bench-branching.hpp
@@ -10,6 +10,6 @@ auto measure_branching_dynamics(scip::Model model) -> Metrics;
 auto measure_branching_rule(scip::Model model) -> Metrics;
 
 auto benchmark_branching(scip::Model model, Tags tags = {}) -> Result;
-auto benchmark_branching(std::vector<scip::Model> models, Tags tags = {}) -> std::vector<Result>;
+auto benchmark_branching(ModelGenerator gen, std::size_t n, Tags tags = {}) -> std::vector<Result>;
 
 }  // namespace ecole::benchmark
