ab884f67f19135f3fedba192c87bc66e5d9d0607
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-01-28 15:05:02 -0500

    Add benchmark_branching utility

--- libecole/benchmarks/src/benchmark.cpp
+++ libecole/benchmarks/src/benchmark.cpp
@@ -13,7 +13,7 @@ auto InstanceFeatures::from_model(scip::Model const& model) -> InstanceFeatures 
 	return {model.variables().size(), model.constraints().size(), model.nnz()};
 }
 
-auto run(CompetitorMap const& competitors, scip::Model model, Tags tags) -> Result {
+auto benchmark_lambda(CompetitorMap const& competitors, scip::Model model, Tags tags) -> Result {
 	auto result = Result{InstanceFeatures::from_model(model), {}, std::move(tags)};
 	for (auto const& [name, competitor] : competitors) {
 		result.metrics[name] = std::invoke(competitor, model.copy_orig());
@@ -21,23 +21,16 @@ auto run(CompetitorMap const& competitors, scip::Model model, Tags tags) -> Resu
 	return result;
 }
 
-auto run(CompetitorMap const& competitors, scip::Model model) -> Result {
-	return run(competitors, std::move(model), {});
-}
-
-auto run(CompetitorMap const& competitors, std::vector<scip::Model> models, Tags tags) -> std::vector<Result> {
+auto benchmark_lambda(CompetitorMap const& competitors, std::vector<scip::Model> models, Tags tags)
+	-> std::vector<Result> {
 	auto results = std::vector<Result>{};
 	results.reserve(models.size());
 	std::transform(
 		std::make_move_iterator(models.begin()),
 		std::make_move_iterator(models.end()),
 		std::back_inserter(results),
-		[&](scip::Model&& model) { return run(competitors, std::move(model), tags); });
+		[&](scip::Model&& model) { return benchmark_lambda(competitors, std::move(model), tags); });
 	return results;
-}
-
-auto run(CompetitorMap const& competitors, std::vector<scip::Model> models) -> std::vector<Result> {
-	return run(competitors, std::move(models), {});
 }
 
 }  // namespace ecole::benchmark
