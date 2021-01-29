137fc32712282bd567b13f8e9b5b3fa2247588de
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-01-29 09:48:22 -0500

    Add branching root node information

--- libecole/benchmarks/src/benchmark.cpp
+++ libecole/benchmarks/src/benchmark.cpp
@@ -3,22 +3,38 @@
 #include <iterator>
 #include <utility>
 
+#include "ecole/dynamics/branching.hpp"
 #include "ecole/scip/model.hpp"
 
 #include "benchmark.hpp"
 
 namespace ecole::benchmark {
 
-auto InstanceFeatures::from_model(scip::Model const& model) -> InstanceFeatures {
-	return {model.variables().size(), model.constraints().size(), model.nnz()};
+auto InstanceFeatures::from_model(scip::Model model) -> InstanceFeatures {
+	// Get model to the root note to extract root node info
+	auto dyn = dynamics::BranchingDynamics{};
+	dyn.reset_dynamics(model);
+	// FIXME in practice there is might be LP even if we never branch. Should use SCIP_EVENTTYPE_FIRSTLPSOLVED
+	if (model.get_stage() != SCIP_STAGE_SOLVING) {
+		return {
+			model.variables().size(),
+			model.constraints().size(),
+		};
+	}
+	return {
+		model.variables().size(),
+		model.constraints().size(),
+		model.nnz(),
+		model.lp_columns().size(),
+		model.lp_rows().size()};
 }
 
 auto benchmark_lambda(CompetitorMap const& competitors, scip::Model model, Tags tags) -> Result {
-	auto result = Result{InstanceFeatures::from_model(model), {}, std::move(tags)};
+	auto metrics = MetricsMap{};
 	for (auto const& [name, competitor] : competitors) {
-		result.metrics[name] = std::invoke(competitor, model.copy_orig());
+		metrics[name] = std::invoke(competitor, model.copy_orig());
 	}
-	return result;
+	return {InstanceFeatures::from_model(std::move(model)), std::move(metrics), std::move(tags)};
 }
 
 auto benchmark_lambda(CompetitorMap const& competitors, ModelGenerator gen, std::size_t n, Tags tags)
--- libecole/benchmarks/src/benchmark.hpp
+++ libecole/benchmarks/src/benchmark.hpp
@@ -15,11 +15,13 @@ namespace ecole::benchmark {
 struct InstanceFeatures {
 	std::size_t n_vars = 0;
 	std::size_t n_cons = 0;
-	std::size_t n_nonzero = 0;
+	std::size_t root_nnz = 0;
+	std::size_t root_n_cols = 0;
+	std::size_t root_n_rows = 0;
 
-	static auto from_model(scip::Model const& model) -> InstanceFeatures;
+	static auto from_model(scip::Model model) -> InstanceFeatures;
 
-	NLOHMANN_DEFINE_TYPE_INTRUSIVE(InstanceFeatures, n_vars, n_cons, n_nonzero);
+	NLOHMANN_DEFINE_TYPE_INTRUSIVE(InstanceFeatures, n_vars, n_cons, root_nnz, root_n_cols, root_n_rows);
 };
 
 struct Metrics {
