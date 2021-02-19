924876b931a91b988cacbce2710124bd62e75fbf
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-02-19 12:24:29 -0500

    Fix conda flag reading

--- cmake/Conda.cmake
+++ cmake/Conda.cmake
@@ -14,26 +14,22 @@ endif()
 
 # Utility to set the language and linker flags.
 function(set_flags LANG FLAGS)
-	set(BUILD "${ARGV2}")  # Optional build type
-	if(BUILD)
-		set(
-			CMAKE_${LANG}_FLAGS_${BUILD} "${FLAGS}"
-			CACHE STRING "Flags used by ${LANG} during ${BUILD} builds."
-		)
-		MARK_AS_ADVANCED(CMAKE_${LANG}_FLAGS_${BUILD})
-	else()
-		set(
-			CMAKE_${LANG}_FLAGS "${FLAGS}"
-			CACHE STRING "Flags used by ${LANG} during all builds."
-		)
-		MARK_AS_ADVANCED(CMAKE_${LANG}_FLAGS)
-	endif()
+set(BUILD "${ARGV2}")  # Optional build type
+	set(
+		CMAKE_${LANG}_FLAGS_${BUILD} "${FLAGS}"
+		CACHE STRING "Flags used by ${LANG} during ${BUILD} builds."
+	)
+	MARK_AS_ADVANCED(CMAKE_${LANG}_FLAGS_${BUILD})
 endfunction()
 
 # Define the CondaDebug build type
 set_flags(Fortran "$ENV{DEBUG_FFLAGS}" CONDADEBUG)
 set_flags(C "$ENV{DEBUG_CFLAGS} $ENV{DEBUG_CPPFLAGS}" CONDADEBUG)
 set_flags(CXX "$ENV{DEBUG_CXXFLAGS} $ENV{DEBUG_CPPFLAGS}" CONDADEBUG)
+# Unset the environment flags in order to prevent CMake from reading them
+set(ENV{DEBUG_FFLAGS} "")
+set(ENV{DEBUG_CFLAGS} "")
+set(ENV{DEBUG_CXXFLAGS} "")
 
 # Define the CondaRelease build type
 set_flags(Fortran "$ENV{FFLAGS}" CONDARELEASE)
@@ -42,11 +38,8 @@ set_flags(CXX "$ENV{CXXFLAGS} $ENV{CPPFLAGS}" CONDARELEASE)
 set_flags(EXE_LINKER "$ENV{LDFLAGS}" CONDARELEASE)
 set_flags(SHARED_LINKER "$ENV{LDFLAGS}" CONDARELEASE)
 set_flags(MODULE_LINKER "$ENV{LDFLAGS}" CONDARELEASE)
-
-# Reset the shared flags
-set_flags(Fortran "")
-set_flags(C "")
-set_flags(CXX "")
-set_flags(EXE_LINKER "")
-set_flags(SHARED_LINKER "")
-set_flags(MODULE_LINKER "")
+# Unset the environment flags in order to prevent CMake from reading them
+set(ENV{FFLAGS} "")
+set(ENV{CFLAGS} "")
+set(ENV{CXXFLAGS} "")
+set(ENV{LDFLAGS} "")
