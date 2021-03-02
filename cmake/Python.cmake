15f66c320ae22fd51019d281756b00b9899ba93c
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-03-02 10:22:17 -0500

    Find Python from PATH with no symlink

--- cmake/Python.cmake
+++ cmake/Python.cmake
@@ -1,2 +1,8 @@
-# We want Python and Numpy
+# Find Python interpreter from the path and don't resolve symlinks.
+execute_process(
+	COMMAND "python3" "-c" "import sys; print(sys.executable)"
+	OUTPUT_VARIABLE Python_EXECUTABLE
+	OUTPUT_STRIP_TRAILING_WHITESPACE
+)
+# Let CMake find the full package from the interpreter
 find_package(Python COMPONENTS Interpreter Development NumPy REQUIRED)
