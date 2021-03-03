94cba45600c692aacd065b094daf0139ef4d57db
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-03-03 16:16:15 -0500

    Add ECOLE_BUILD_BENCHMARKS and ECOLE_BUILD_TESTS CMake options

--- cmake/DefaultSettings.cmake
+++ cmake/DefaultSettings.cmake
@@ -19,18 +19,20 @@ endfunction()
 
 # Set default common for all cases
 macro(set_common_defaults)
+	option(CMAKE_EXPORT_COMPILE_COMMANDS "Generate compile_commands.json for tools" ON)
 	option(CMAKE_POSITION_INDEPENDENT_CODE "Position Independent Code for building shared libraries." ON)
 	option(CMAKE_VISIBILITY_INLINES_HIDDEN "Hidden symbol visibility for inline functions in shared libraries" ON)
 	set(CMAKE_CXX_VISIBILITY_PRESET hidden CACHE STRING "Hidden visibility of symbols in shared libraries.")
-	option(ECOLE_BENCHMARK "Build Ecole benchmakrs" OFF)
 endmacro()
 
 
 # Set of defaults for Ecole users
 macro(set_user_defaults)
 	set_common_defaults()
-	option(ENABLE_IPO "Enable Interprocedural Optimization, aka Link Time Optimization (LTO)" ON)
 	set_default_build_type(RelWithDebInfo)
+	option(ENABLE_IPO "Enable Interprocedural Optimization, aka Link Time Optimization (LTO)" ON)
+	option(ECOLE_BUILD_BENCHMARKS "Build Ecole benchmarks" OFF)
+	option(ECOLE_BUILD_TESTS "Build Ecole tests" OFF)
 endmacro()
 
 
@@ -40,10 +42,8 @@ macro(set_developer_defaults)
 	set_default_build_type(Debug)
 
 	option(ENABLE_PYTHON_VENV "Install Ecole Python library in a virtual environment" ON)
-	option(BUILD_TESTING "Build tests in Ecole" ON)
-
-	# Generate compile_commands.json to make it easier to work with clang based tools
-	set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+	option(ECOLE_BUILD_BENCHMARKS "Build Ecole benchmarks" ON)
+	option(ECOLE_BUILD_TESTS "Build Ecole tests" ON)
 
 	# Enable compiler cache if found
 	find_program(CCACHE ccache)
--- libecole/benchmarks/src/branching/index-branchrule.hpp
+++ libecole/benchmarks/src/branching/index-branchrule.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <utility>
+
 #include "branching/lambda-branchrule.hpp"
 
 namespace ecole::scip {
