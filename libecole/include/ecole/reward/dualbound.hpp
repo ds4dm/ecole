f5a74b0f5a6a61e374a9f3e7e5dea40abe46081b
beljadid <ahmed.beljadid@gmail.com>
2021-03-04 17:20:58 -0700

    Update dualbound.hpp

--- libecole/include/ecole/reward/dualbound.hpp
+++ libecole/include/ecole/reward/dualbound.hpp
@@ -3,6 +3,8 @@
 #include "ecole/reward/abstract.hpp"
 #include "ecole/scip/type.hpp"
 
+#define infinity   1e+20
+
 namespace ecole::reward {
 
 class DualBound : public RewardFunction {
@@ -11,8 +13,8 @@ public:
 	Reward extract(scip::Model& model, bool done = false) override;
 
 private:
-	scip::real dual_bound_value = 0.0;
-//	scip::real dual_bound_value = Infinity;
+//	scip::real dual_bound_value = 0.0;
+	scip::real dual_bound_value = - infinity;
 };
 
 }  // namespace ecole::reward
