9fea0e2ed3645d1e441a432f4ebff82fd4df0d2d
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-04-08 11:35:56 -0400

    Don't resolve Python if already set

--- cmake/Python.cmake
+++ cmake/Python.cmake
@@ -1,8 +1,11 @@
-# Find Python interpreter from the path and don't resolve symlinks.
-execute_process(
-	COMMAND "python3" "-c" "import sys; print(sys.executable)"
-	OUTPUT_VARIABLE Python_EXECUTABLE
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
+if(NOT DEFINED Python_EXECUTABLE)
+	# Find Python interpreter from the path and don't resolve symlinks
+	# TODO: this is no longer required whrn switching to scikit-build
+	execute_process(
+		COMMAND "python3" "-c" "import sys; print(sys.executable)"
+		OUTPUT_VARIABLE Python_EXECUTABLE
+		OUTPUT_STRIP_TRAILING_WHITESPACE
+	)
+endif()
 # Let CMake find the full package from the interpreter
 find_package(Python COMPONENTS Interpreter Development NumPy REQUIRED)
