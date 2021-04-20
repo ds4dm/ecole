50a33920a1a95837a35514f2c3e20d45ec9173e0
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-04-20 09:37:12 -0400

    Read skbuild cmake args from env variable

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 import pathlib
 import re
 import sys
+import os
+import shlex
 
-from skbuild import setup
+import skbuild
 
 __dir__ = pathlib.Path(__file__).resolve().parent
 
@@ -26,7 +28,7 @@ if (sys.version_info.major == 3) and (sys.version_info.minor <= 6):
     install_requires += ["typing_extensions"]
 
 
-setup(
+skbuild.setup(
     name="ecole",
     author="Antoine Prouvost et al.",
     version=get_version(__dir__ / "VERSION"),
@@ -38,6 +40,10 @@ setup(
     package_data={"ecole": ["py.typed"]},
     cmake_languages=["CXX"],
     cmake_install_dir="python/src",
+    # FIXME No way to pass cmake argument to scikit-build through pip (for now)
+    # https://github.com/scikit-build/scikit-build/issues/479
+    # So we read them from an environment variable
+    cmake_args=shlex.split(os.environ.get("CMAKE_ARGS", "")),
     zip_safe=False,
     python_requires=">=3.6",
     install_requires=install_requires,
