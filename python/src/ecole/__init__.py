36bb9aba7540ca604bd82521eee47d385a48daec
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-04-20 11:12:43 -0400

    ecole.__version__ from libecole

--- python/src/ecole/__init__.py
+++ python/src/ecole/__init__.py
@@ -1,19 +1,8 @@
 import sys
 
-
-# Ask Python about our version (avoids templating this file)
-if (sys.version_info.major == 3) and (sys.version_info.minor <= 7):
-    import pkg_resources
-
-    __version__ = pkg_resources.get_distribution(__name__).version
-else:
-    import importlib.metadata
-
-    __version__ = importlib.metadata.version(__name__)
-
-
 from ecole.core import RandomEngine, seed, spawn_random_engine, Exception
 
+import ecole.version
 import ecole.data
 import ecole.observation
 import ecole.reward
@@ -22,3 +11,5 @@ import ecole.scip
 import ecole.instance
 import ecole.dynamics
 import ecole.environment
+
+__version__ = ecole.version.__version__
