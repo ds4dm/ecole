e33e5b85f877451251e7b1278906a3a0c9f95ba9
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-04-22 11:27:08 -0400

    Refactor docs/build.sh for sourcing

--- docs/build.sh
+++ docs/build.sh
@@ -1,26 +1,56 @@
 #!/usr/bin/env bash
 
-# Fail fast
-set -o errexit
-set -o pipefail
-set -o nounset
 
-# Directory of this file
-__DIR__="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# Top of the repository in which this file is
-__ECOLE_DIR__="$(git -C "${__DIR__}" rev-parse --show-toplevel)"
+# Print in yellow with a new line.
+function echo_yellow {
+	local -r yellow="\033[1;33m"
+	local -r nc="\033[0m"
+	printf "${yellow}$*${nc}\n"
+}
 
 
-function main {
+# Logging entry point
+function log {
+	echo_yellow "$@"
+}
+
+
+# Wrap calls to manage verbosity, dry-run, ...
+function execute {
+	log "$@"
+	"$@"
+}
+
+
+# Build the html documentation.
+function build_doc {
+	if [ ${warnings_as_errors} = "true" ]; then
+		local sphinx_args+=("-W")
+	fi
+	execute python -m sphinx "${sphinx_args[@]}" -b html "${source_doc_dir}" "${build_doc_dir}/html"
+}
+
+
+# Build the html documentation and test the code snippets and links,
+function test_doc {
+	if [ ${warnings_as_errors} = "true" ]; then
+		local sphinx_args+=("-W")
+	fi
+	execute python -m sphinx "${sphinx_args[@]}" -b linkcheck "${source_doc_dir}" "${build_doc_dir}/html"
+	execute python -m sphinx "${sphinx_args[@]}" -b doctest "${source_doc_dir}" "${build_doc_dir}/html"
+}
+
+
+# Parse arguments, build, and test documentation.
+function doc_main {
 	# Fail if there are warnings.
 	local warnings_as_errors="false"
 	# Additional testing of the documentation.
-	local enable_testing="false"
+	local test="false"
 	# Where to find sphinx conf.py.
-	local source_dir="${__ECOLE_DIR__}/docs"
+	local source_doc_dir="${__ECOLE_DIR__}/docs"
 	# Where to output the doc.
-	local build_dir="${__ECOLE_DIR__}/build/doc"
+	local build_doc_dir="${__ECOLE_DIR__}/build/docs"
 	# Other arguments to pass to sphinx.
 	local sphinx_args=()
 
@@ -28,28 +58,20 @@ function main {
 	while [[ $# -gt 0 ]]; do
 		local arg="${1}"
 		case "${arg}" in
-			--differential)
-				differential="origin/master"
-				shift
-				;;
-			--differential=*)
-				differential="${arg#*=}"
-				shift
-				;;
-			--warnings-as-errors)
+			--warnings-as-errors|--wae)
 				warnings_as_errors="true"
 				shift
 				;;
-			--enable-testing)
-				enable_testing="true"
+			--test)
+				test="true"
 				shift
 				;;
-			--source-dir=*)
-				source_dir="${arg#*=}"
+			--source-doc-dir=*)
+				source_doc_dir="${arg#*=}"
 				shift
 				;;
-			--build-dir=*)
-				build_dir="${arg#*=}"
+			--build-doc-dir=*)
+				build_doc_dir="${arg#*=}"
 				shift
 				;;
 			*)
@@ -60,14 +82,26 @@ function main {
 	done
 
 	# Build according to testing/error policy
-	sphinx_args+=("$([ ${warnings_as_errors} = "true" ] && printf -- '-W')")
-	if [ ${enable_testing} = "true" ]; then
-		python -m sphinx "${sphinx_args}" -b html "${source_dir}" "${build_dir}/html"
-		python -m sphinx "${sphinx_args}" -b linkcheck "${source_dir}" "${build_dir}"
-		python -m sphinx "${sphinx_args}" -b doctest "${source_dir}" "${build_dir}"
-	else
-		python -m pshinx "${sphinx_args}" -b html "${source_dir}" "${build_dir}/html"
+	build_doc
+	if [ ${test} = "true" ]; then
+		test_doc
 	fi
 }
 
-main "$@"
+# Run the main when script is not being sourced
+if [[ "${BASH_SOURCE[0]}" = "${0}" ]] ; then
+
+	# Fail fast
+	set -o errexit
+	set -o pipefail
+	set -o nounset
+
+	# Directory of this file
+	__DIR__="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+	# Top of the repository in which this file is
+	__ECOLE_DIR__="$(git -C "${__DIR__}" rev-parse --show-toplevel)"
+
+	doc_main "$@"
+
+fi
