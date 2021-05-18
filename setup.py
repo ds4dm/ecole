0e1c355248bcb76295197afa8236335bae4d34ee
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-05-18 11:05:56 -0400

    Filter CMAKE_ARGS from conda-build in scikit-build

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ import sys
 import os
 import shlex
 
+from typing import List
+
 import skbuild
 
 __dir__ = pathlib.Path(__file__).resolve().parent
@@ -30,6 +32,15 @@ def get_version(version_file: pathlib.Path) -> str:
     return "{major}.{minor}.{patch}{pre}{post}{dev}".format(**version_dict)
 
 
+def get_cmake_args() -> List[str]:
+    """Return the list of extra CMake arguments from the environment."""
+    cmake_args = shlex.split(os.environ.get("CMAKE_ARGS", ""))
+    if "CONDA_BUILD" in os.environ:
+        install_re = re.compile(r"-D\s*CMAKE_INSTALL.*")
+        cmake_args = [a for a in cmake_args if not install_re.search(a)]
+    return cmake_args
+
+
 install_requires = ["numpy>=1.4"]
 if (sys.version_info.major == 3) and (sys.version_info.minor <= 6):
     install_requires += ["typing_extensions"]
@@ -52,7 +63,7 @@ skbuild.setup(
     # FIXME No way to pass cmake argument to scikit-build through pip (for now)
     # https://github.com/scikit-build/scikit-build/issues/479
     # So we read them from an environment variable
-    cmake_args=shlex.split(os.environ.get("CMAKE_ARGS", "")),
+    cmake_args=get_cmake_args(),
     zip_safe=False,
     python_requires=">=3.6",
     install_requires=install_requires,
