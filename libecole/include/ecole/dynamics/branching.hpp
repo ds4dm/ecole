5d1979a1f595aab7f5c230fe7a225542ee72c8bc
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-05-25 12:40:07 -0400

    Cleanup BranchingDynamics

--- libecole/include/ecole/dynamics/branching.hpp
+++ libecole/include/ecole/dynamics/branching.hpp
@@ -19,7 +19,7 @@ public:
 
 	std::tuple<bool, ActionSet> reset_dynamics(scip::Model& model) override;
 
-	std::tuple<bool, ActionSet> step_dynamics(scip::Model& model, std::size_t const& action) override;
+	std::tuple<bool, ActionSet> step_dynamics(scip::Model& model, std::size_t const& var_idx) override;
 };
 
 }  // namespace ecole::dynamics
