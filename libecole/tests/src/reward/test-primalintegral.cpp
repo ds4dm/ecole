cd405dfe28975a18c65ccd82d505dfcd67f11546
jdumouchelle <jdumouch@uwaterloo.ca>
2021-05-25 10:26:54 -0400

    tests for primal and primal-dual integral  reward functions

---
+++ libecole/tests/src/reward/test-primalintegral.cpp
@@ -0,0 +1,22 @@
+#include <catch2/catch.hpp>
+
+#include "ecole/reward/primalintegral.hpp"
+
+#include "conftest.hpp"
+#include "reward/unit-tests.hpp"
+
+using namespace ecole;
+
+TEST_CASE("PrimalIntegral unit tests", "[unit][reward]") {
+	reward::unit_tests(reward::PrimalIntegral{});
+}
+
+TEST_CASE("PrimalIntegral returns the difference in dual integral between two states", "[reward]") {
+	auto reward_func = reward::PrimalIntegral{};
+	auto model = get_model();  // a non-trivial instance is loaded
+
+	SECTION("PrimalIntegral is positive before presolving") {
+		reward_func.before_reset(model);
+		REQUIRE(reward_func.extract(model) > 0);
+	}
+}
