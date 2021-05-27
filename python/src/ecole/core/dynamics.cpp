c48a06a4e7737f690b453455b3a98c058fd0c177
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-05-27 16:20:59 -0400

    Refactor dynamics bindings

--- python/src/ecole/core/dynamics.cpp
+++ python/src/ecole/core/dynamics.cpp
@@ -1,5 +1,4 @@
-#include <memory>
-#include <string>
+#include <utility>
 
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
@@ -15,25 +14,58 @@ namespace ecole::dynamics {
 
 namespace py = pybind11;
 
-template <typename Dynamics> auto dynamics_class(py::module_ const& m, char const* name) {
-	return py::class_<Dynamics>(m, name)  //
-		.def("reset_dynamics", &Dynamics::reset_dynamics, py::arg("model"), py::call_guard<py::gil_scoped_release>())
-		.def(
+template <typename Class, typename... ClassArgs> struct dynamics_class : public pybind11::class_<Class, ClassArgs...> {
+	using pybind11::class_<Class, ClassArgs...>::class_;
+
+	/** Bind reset_dynamics */
+	template <typename... Args> auto def_reset_dynamics(Args&&... args) -> auto& {
+		this->def(
+			"reset_dynamics",
+			&Class::reset_dynamics,
+			py::arg("model"),
+			py::call_guard<py::gil_scoped_release>(),
+			std::forward<Args>(args)...);
+		return *this;
+	}
+
+	/** Bind step_dynamics */
+	template <typename... Args> auto def_step_dynamics(Args&&... args) -> auto& {
+		this->def(
 			"step_dynamics",
-			&Dynamics::step_dynamics,
+			&Class::step_dynamics,
 			py::arg("model"),
 			py::arg("action"),
-			py::call_guard<py::gil_scoped_release>())
-		.def("set_dynamics_random_state", &Dynamics::set_dynamics_random_state, py::arg("model"), py::arg("random_engine"));
-}
+			py::call_guard<py::gil_scoped_release>(),
+			std::forward<Args>(args)...);
+		return *this;
+	}
+
+	/** Bind set_dynamics_random_state */
+	template <typename... Args> auto def_set_dynamics_random_state(Args&&... args) -> auto& {
+		this->def(
+			"set_dynamics_random_state",
+			&Class::set_dynamics_random_state,
+			py::arg("model"),
+			py::arg("random_engine"),
+			py::call_guard<py::gil_scoped_release>(),
+			std::forward<Args>(args)...);
+		return *this;
+	}
+};
 
 void bind_submodule(pybind11::module_ const& m) {
 	m.doc() = "Ecole collection of environment dynamics.";
 
-	dynamics_class<BranchingDynamics>(m, "BranchingDynamics")  //
+	dynamics_class<BranchingDynamics>{m, "BranchingDynamics"}
+		.def_reset_dynamics()
+		.def_step_dynamics()
+		.def_set_dynamics_random_state()
 		.def(py::init<bool>(), py::arg("pseudo_candidates") = false);
 
-	dynamics_class<ConfiguringDynamics>(m, "ConfiguringDynamics")  //
+	dynamics_class<ConfiguringDynamics>{m, "ConfiguringDynamics"}
+		.def_reset_dynamics()
+		.def_step_dynamics()
+		.def_set_dynamics_random_state()
 		.def(py::init<>());
 }
 
