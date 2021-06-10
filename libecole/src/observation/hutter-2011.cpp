edba1d8c4ee8f8970d04dac3fcc42779b7488972
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-06-10 11:11:48 -0400

    Fix formatting in Hutter

--- libecole/src/observation/hutter-2011.cpp
+++ libecole/src/observation/hutter-2011.cpp
@@ -73,94 +73,92 @@ template <typename Tensor> void set_constraint_degrees(Tensor&& out, scip::Model
 }
 
 /*
-* Solves the LP relaxation of a model by making a copy, and setting all its variables continuous.
-*/
+ * Solves the LP relaxation of a model by making a copy, and setting all its variables continuous.
+ */
 auto solve_lp_relaxation(scip::Model const& model) {
-    auto relax_model = model.copy();
+	auto relax_model = model.copy();
 	auto* const relax_scip = relax_model.get_scip_ptr();
-    auto* const variables = SCIPgetVars(relax_scip);
-    int nb_variables = SCIPgetNVars(relax_scip);
-    SCIP_Bool infeasible;
-    
-    // Change active variables to continuous
-    for (std::size_t var_idx = 0; var_idx < static_cast<std::size_t>(nb_variables); ++var_idx) {
-        scip::call(SCIPchgVarType, relax_scip, variables[var_idx], SCIP_VARTYPE_CONTINUOUS, &infeasible);
-    }
-    
-    // Change constraint variables to continuous
-    int nb_cons_variables;
-    SCIP_Bool success;
-	for (auto* const constraint : relax_model.constraints()) {
-        scip::call(SCIPgetConsNVars, relax_scip, constraint, &nb_cons_variables, &success);
-        auto cons_variables = std::vector<SCIP_VAR*>(static_cast<std::size_t>(nb_cons_variables));
-        scip::call(SCIPgetConsVars, relax_scip, constraint, cons_variables.data(), nb_cons_variables, &success);
-        
-        for (std::size_t var_idx = 0; var_idx < static_cast<std::size_t>(nb_cons_variables); ++var_idx) {
-            scip::call(SCIPchgVarType, relax_scip, cons_variables[var_idx], SCIP_VARTYPE_CONTINUOUS, &infeasible);
-        }
+	auto* const variables = SCIPgetVars(relax_scip);
+	int nb_variables = SCIPgetNVars(relax_scip);
+	SCIP_Bool infeasible;
+
+	// Change active variables to continuous
+	for (std::size_t var_idx = 0; var_idx < static_cast<std::size_t>(nb_variables); ++var_idx) {
+		scip::call(SCIPchgVarType, relax_scip, variables[var_idx], SCIP_VARTYPE_CONTINUOUS, &infeasible);
 	}
-    
-    // Solve the LP
-    scip::call(SCIPsolve, relax_scip);
-    
-    // Collect the solution
-    // Note: technically this is the solution with respect to the copy model's active variables
-    // Hopefully this should match 1-1 the original model's active variables?
-    SCIP_SOL* optimal_sol = SCIPgetBestSol(relax_scip);
-    SCIP_Real optimal_value = SCIPgetSolOrigObj(relax_scip, optimal_sol);
-    auto optimal_sol_coefs = std::vector<SCIP_Real>(static_cast<std::size_t>(nb_variables));
-    scip::call(SCIPgetSolVals, relax_scip, optimal_sol, nb_variables, variables, optimal_sol_coefs.data());
-    
-    return std::tuple{optimal_sol_coefs, optimal_value};
+
+	// Change constraint variables to continuous
+	int nb_cons_variables;
+	SCIP_Bool success;
+	for (auto* const constraint : relax_model.constraints()) {
+		scip::call(SCIPgetConsNVars, relax_scip, constraint, &nb_cons_variables, &success);
+		auto cons_variables = std::vector<SCIP_VAR*>(static_cast<std::size_t>(nb_cons_variables));
+		scip::call(SCIPgetConsVars, relax_scip, constraint, cons_variables.data(), nb_cons_variables, &success);
+
+		for (std::size_t var_idx = 0; var_idx < static_cast<std::size_t>(nb_cons_variables); ++var_idx) {
+			scip::call(SCIPchgVarType, relax_scip, cons_variables[var_idx], SCIP_VARTYPE_CONTINUOUS, &infeasible);
+		}
+	}
+
+	// Solve the LP
+	scip::call(SCIPsolve, relax_scip);
+
+	// Collect the solution
+	// Note: technically this is the solution with respect to the copy model's active variables
+	// Hopefully this should match 1-1 the original model's active variables?
+	SCIP_SOL* optimal_sol = SCIPgetBestSol(relax_scip);
+	SCIP_Real optimal_value = SCIPgetSolOrigObj(relax_scip, optimal_sol);
+	auto optimal_sol_coefs = std::vector<SCIP_Real>(static_cast<std::size_t>(nb_variables));
+	scip::call(SCIPgetSolVals, relax_scip, optimal_sol, nb_variables, variables, optimal_sol_coefs.data());
+
+	return std::tuple{optimal_sol_coefs, optimal_value};
 }
-    
 
 template <typename Tensor> void set_lp_based_features(Tensor&& out, scip::Model const& model) {
-    auto [lp_solution, lp_objective] = solve_lp_relaxation(model);
-    
-    // Compute the integer slack vector
-    auto variables = model.variables();
-    auto* const scip = const_cast<SCIP*>(model.get_scip_ptr());
-    int nb_integer_variables = SCIPgetNBinVars(scip) + SCIPgetNIntVars(scip);
-    
-    if (nb_integer_variables > 0) {
-        // Compute the integer slack vector
-        auto integer_slack = std::vector<SCIP_Real>(static_cast<std::size_t>(nb_integer_variables));
-        std::size_t int_var_idx = 0;
-        for (std::size_t var_idx = 0; var_idx < variables.size(); ++var_idx) {
-            if (SCIPvarIsIntegral(variables[var_idx])) {
-                auto lp_solution_coef = lp_solution[int_var_idx];
-                integer_slack[int_var_idx] = std::abs(lp_solution_coef - std::round(lp_solution_coef));
-                int_var_idx++;
-            }
-        }
+	auto [lp_solution, lp_objective] = solve_lp_relaxation(model);
 
-        // Compute statistics of the integer slack vector
-        auto const slack_stats = utility::compute_stats(integer_slack);
-        SCIP_Real slack_l2_norm = 0;
-        for (auto const coefficient : integer_slack) {
-            slack_l2_norm += utility::square(coefficient);
-        }
+	// Compute the integer slack vector
+	auto variables = model.variables();
+	auto* const scip = const_cast<SCIP*>(model.get_scip_ptr());
+	int nb_integer_variables = SCIPgetNBinVars(scip) + SCIPgetNIntVars(scip);
 
-        out[idx(Features::lp_slack_mean)] = slack_stats.mean;
-        out[idx(Features::lp_slack_max)] = slack_stats.max;
-        out[idx(Features::lp_slack_l2)] = slack_l2_norm;
-    }
-    else {
-        out[idx(Features::lp_slack_mean)] = 0;
-        out[idx(Features::lp_slack_max)] = 0;
-        out[idx(Features::lp_slack_l2)] = 0;
-    }
-    out[idx(Features::lp_objective_value)] = lp_objective;
+	if (nb_integer_variables > 0) {
+		// Compute the integer slack vector
+		auto integer_slack = std::vector<SCIP_Real>(static_cast<std::size_t>(nb_integer_variables));
+		std::size_t int_var_idx = 0;
+		for (auto& variable : variables) {
+			if (SCIPvarIsIntegral(variable)) {
+				auto lp_solution_coef = lp_solution[int_var_idx];
+				integer_slack[int_var_idx] = std::abs(lp_solution_coef - std::round(lp_solution_coef));
+				int_var_idx++;
+			}
+		}
+
+		// Compute statistics of the integer slack vector
+		auto const slack_stats = utility::compute_stats(integer_slack);
+		SCIP_Real slack_l2_norm = 0;
+		for (auto const coefficient : integer_slack) {
+			slack_l2_norm += utility::square(coefficient);
+		}
+
+		out[idx(Features::lp_slack_mean)] = slack_stats.mean;
+		out[idx(Features::lp_slack_max)] = slack_stats.max;
+		out[idx(Features::lp_slack_l2)] = slack_l2_norm;
+	} else {
+		out[idx(Features::lp_slack_mean)] = 0;
+		out[idx(Features::lp_slack_max)] = 0;
+		out[idx(Features::lp_slack_l2)] = 0;
+	}
+	out[idx(Features::lp_objective_value)] = lp_objective;
 }
 
-auto extract_features(scip::Model& model) {    
+auto extract_features(scip::Model& model) {
 	xt::xtensor<value_type, 1> observation({Hutter2011Obs::n_features});
 	set_problem_size(observation, model);
 	set_variable_degrees(observation, model);
 	set_constraint_degrees(observation, model);
-    set_lp_based_features(observation, model);
-    
+	set_lp_based_features(observation, model);
+
 	return observation;
 }
 
