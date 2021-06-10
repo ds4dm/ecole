4ddd85c7d9ee84382e3eeaf13b60267fd1df262c
jdumouchelle <jdumouch@uwaterloo.ca>
2021-06-10 18:18:11 -0400

    include event handler in boundintegral cpp file

--- libecole/src/reward/boundintegral.cpp
+++ libecole/src/reward/boundintegral.cpp
@@ -1,7 +1,9 @@
+#include <chrono>
+
 #include "scip/scip.h"
 #include "scip/type_event.h"
+#include <objscip/objeventhdlr.h>
 
-#include "ecole/reward/integral_eventhdlr.hpp"
 #include "ecole/reward/boundintegral.hpp"
 #include "ecole/scip/model.hpp"
 #include "ecole/utility/chrono.hpp"
@@ -9,6 +11,155 @@
 namespace ecole::reward {
 
 namespace {
+
+/*
+ * Helper functions for IntegralEventHandler
+ */
+
+auto time_now(bool wall) -> std::chrono::nanoseconds {
+	if (wall) {
+		return std::chrono::steady_clock::now().time_since_epoch();
+	}
+	return utility::cpu_clock::now().time_since_epoch();
+}
+
+/* Gets the primal bound of the scip model */
+auto get_primal_bound(SCIP* scip) {
+	switch (SCIPgetStage(scip)) {
+	case SCIP_STAGE_TRANSFORMED:
+	case SCIP_STAGE_INITPRESOLVE:
+	case SCIP_STAGE_PRESOLVING:
+	case SCIP_STAGE_EXITPRESOLVE:
+	case SCIP_STAGE_PRESOLVED:
+	case SCIP_STAGE_INITSOLVE:
+	case SCIP_STAGE_SOLVING:
+	case SCIP_STAGE_SOLVED:
+		return SCIPgetPrimalbound(scip);
+	default:
+		return SCIPinfinity(scip);
+	}
+}
+
+/* Gets the dual bound of the scip model */
+auto get_dual_bound(SCIP* scip) {
+	switch (SCIPgetStage(scip)) {
+	case SCIP_STAGE_TRANSFORMED:
+	case SCIP_STAGE_INITPRESOLVE:
+	case SCIP_STAGE_PRESOLVING:
+	case SCIP_STAGE_EXITPRESOLVE:
+	case SCIP_STAGE_PRESOLVED:
+	case SCIP_STAGE_INITSOLVE:
+	case SCIP_STAGE_SOLVING:
+	case SCIP_STAGE_SOLVED:
+		return SCIPgetDualbound(scip);
+	default:
+		return -SCIPinfinity(scip);
+	}
+}
+
+auto is_lp_event(SCIP_EVENTTYPE event) {
+
+	if ((event == SCIP_EVENTTYPE_LPEVENT) | 
+		(event == SCIP_EVENTTYPE_FIRSTLPSOLVED) |
+		(event == SCIP_EVENTTYPE_LPSOLVED)) {
+		return true;
+	}
+	return false;
+}
+
+auto is_bestsol_event(SCIP_EVENTTYPE event) {
+	if (event == SCIP_EVENTTYPE_BESTSOLFOUND) {
+		return true;
+	}
+	return false;
+}
+
+/*
+ * Class definition for IntegralEventHandler
+ */
+
+class IntegralEventHandler : public ::scip::ObjEventhdlr {
+public:
+	IntegralEventHandler(SCIP* scip, bool wall_, bool extract_primal_, bool extract_dual_) :
+		ObjEventhdlr(scip, "ecole::reward::IntegralEventHandler", "Event handler for primal and dual integrals"), 
+		wall{wall_},
+		extract_primal{extract_primal_},
+		extract_dual{extract_dual_} {} 
+	~IntegralEventHandler() override = default;
+	SCIP_DECL_EVENTINIT(scip_init) override;
+	SCIP_DECL_EVENTEXIT(scip_exit) override;
+	SCIP_DECL_EVENTEXEC(scip_exec) override;
+	
+	[[nodiscard]] std::vector<std::chrono::nanoseconds> const& get_times() const noexcept { return times; }
+	[[nodiscard]] std::vector<SCIP_Real> const& get_primal_bounds() const noexcept { return primal_bounds; }
+	[[nodiscard]] std::vector<SCIP_Real> const& get_dual_bounds() const noexcept { return dual_bounds; }
+
+	void extract_metrics(SCIP* scip, SCIP_EVENTTYPE event_type = 0);
+
+private:
+	bool wall;
+	bool extract_primal;
+	bool extract_dual;
+	std::vector<std::chrono::nanoseconds> times;
+	std::vector<SCIP_Real> primal_bounds;
+	std::vector<SCIP_Real> dual_bounds;
+};
+
+/*
+ * Methods for IntegralEventHandler
+ */
+
+/* Catches primal and dual related events */
+SCIP_DECL_EVENTINIT(IntegralEventHandler::scip_init) {
+	if (extract_primal) {
+		SCIP_CALL(SCIPcatchEvent(scip, SCIP_EVENTTYPE_BESTSOLFOUND, eventhdlr, nullptr, nullptr));
+	}
+	if (extract_dual) {
+		SCIP_CALL(SCIPcatchEvent(scip, SCIP_EVENTTYPE_LPEVENT, eventhdlr, nullptr, nullptr));
+	}
+	return SCIP_OKAY;
+}
+
+/* Drops primal and dual related events */
+SCIP_DECL_EVENTEXIT(IntegralEventHandler::scip_exit) {
+	if (extract_primal) {
+		SCIP_CALL(SCIPdropEvent(scip, SCIP_EVENTTYPE_BESTSOLFOUND, eventhdlr, nullptr, -1));
+	}
+	if (extract_dual) {
+		SCIP_CALL(SCIPdropEvent(scip, SCIP_EVENTTYPE_LPEVENT, eventhdlr, nullptr, -1));
+	}
+	return SCIP_OKAY;
+}
+
+/* Calls extract_metrics() to obtain bounds/times at events */
+SCIP_DECL_EVENTEXEC(IntegralEventHandler::scip_exec) {
+	extract_metrics(scip_, SCIPeventGetType(event));
+	return SCIP_OKAY;
+}
+
+/* Gets and adds primal/dual bounds and times to vectors */
+void IntegralEventHandler::extract_metrics(SCIP* scip, SCIP_EVENTTYPE event_type) {
+	if (extract_primal) {
+		if ((is_lp_event(event_type)) | (primal_bounds.size() == 0)) {
+			primal_bounds.push_back(get_primal_bound(scip));
+		} else {
+			primal_bounds.push_back(primal_bounds[primal_bounds.size()-1]);
+		}
+	}
+	if (extract_dual) {
+		if ((is_bestsol_event(event_type)) | (dual_bounds.size() == 0)) {
+			dual_bounds.push_back(get_dual_bound(scip));
+		} else {
+			dual_bounds.push_back(dual_bounds[dual_bounds.size()-1]);
+		}
+	}
+	times.push_back(time_now(scip));
+}
+
+
+/*
+ * Helper functions for BoundIntegral
+ */
 
 auto compute_dual_integral(
 	std::vector<SCIP_Real> const & dual_bounds,
