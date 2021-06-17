168370958d29652267976015f2b513758d08a201
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-06-17 17:15:22 -0400

    Add PrimalSearch C++ environment

---
+++ libecole/include/ecole/environment/primalsearch.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "ecole/dynamics/primalsearch.hpp"
+#include "ecole/environment/environment.hpp"
+#include "ecole/information/nothing.hpp"
+#include "ecole/observation/nodebipartite.hpp"
+#include "ecole/reward/isdone.hpp"
+
+namespace ecole::environment {
+
+template <
+	typename ObservationFunction = observation::NodeBipartite,
+	typename RewardFunction = reward::IsDone,
+	typename InformationFunction = information::Nothing>
+using Branching = Environment<dynamics::PrimalSearchDynamics, ObservationFunction, RewardFunction, InformationFunction>;
+
+}  // namespace ecole::environment
