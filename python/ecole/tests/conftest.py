301451e44215181e586c254d28f023849d02280a
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-11-15 16:35:34 -0500

    Switch filepath to pathlib

--- python/ecole/tests/conftest.py
+++ python/ecole/tests/conftest.py
@@ -39,7 +39,7 @@ def problem_file():
 @pytest.fixture
 def model(problem_file):
     """Return a Model object with a valid problem."""
-    model_obj = ecole.scip.Model.from_file(str(problem_file))
+    model_obj = ecole.scip.Model.from_file(problem_file)
     model_obj.disable_cuts()
     model_obj.disable_presolve()
     return model_obj
